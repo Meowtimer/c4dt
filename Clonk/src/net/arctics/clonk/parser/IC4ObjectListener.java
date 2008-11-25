853c2e5aa5357771fb326ea6886591d2c8591d33
zokradonh <devnull@localhost>
2008-11-25 18:18:58 +0000

    old deprecated stuff

--- Clonk/src/net/arctics/clonk/parser/IC4ObjectListener.java
+++
@@ -1,9 +0,0 @@
-package net.arctics.clonk.parser;
-
-public interface IC4ObjectListener {
-
-	public void fieldAdded(C4Object obj, C4Field field);
-	public void fieldRemoved(C4Object obj, C4Field field);
-	public void fieldChanged(C4Object obj, C4Field field);
-	
-}
