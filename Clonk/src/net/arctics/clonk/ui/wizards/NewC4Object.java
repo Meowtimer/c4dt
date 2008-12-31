bd249e7f89dfbf644c0386826304d9dbbc4e2657
madeen <devnull@localhost>
2008-12-31 14:51:16 +0000

    deleted doFinish duplicate

--- Clonk/src/net/arctics/clonk/ui/wizards/NewC4Object.java
+++ Clonk/src/net/arctics/clonk/ui/wizards/NewC4Object.java
@@ -48,58 +48,6 @@ public class NewC4Object extends NewClonkFolderWizard implements INewWizard {
 		result.put("$ID$", ((NewC4ObjectPage)page).getObjectID());
 		return result;
 	}
-	
-	/**
-	 * The worker method. It will find the container, create the
-	 * file if missing or just replace its contents, and open
-	 * the editor on the newly created file.
-	 */
-
-	@Override
-	protected void doFinish(
-		String containerName,
-		String fileName,
-		IProgressMonitor monitor)
-		throws CoreException {
-		// create a sample file
-		monitor.beginTask("Creating " + fileName, 1);
-		IWorkspaceRoot root = ResourcesPlugin.getWorkspace().getRoot();
-		IResource resource = root.findMember(new Path(containerName));
-		if (!resource.exists() || !(resource instanceof IContainer)) {
-			throwCoreException("Container \"" + containerName + "\" does not exist.");
-		}
-		IContainer container = (IContainer) resource;
-		final IFolder subContainer = container.getFolder(new Path(fileName));
-		if (!subContainer.exists()) {
-			subContainer.create(IResource.NONE,true,monitor);
-		}
-		try {
-			Enumeration<URL> templates = getTemplateFiles();
-			while (templates.hasMoreElements()) {
-				URL template = templates.nextElement();
-				String templateFile = new Path(template.getFile()).lastSegment();
-				if (templateFile.startsWith("."))
-					continue;
-				InputStream stream = getTemplateStream(template, templateFile);
-				subContainer.getFile(templateFile).create(stream, true, monitor);
-			}
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		monitor.worked(1);
-//		monitor.setTaskName("Opening file for editing...");
-//		getShell().getDisplay().asyncExec(new Runnable() {
-//			public void run() {
-//				IWorkbenchPage page =
-//					PlatformUI.getWorkbench().getActiveWorkbenchWindow().getActivePage();
-//				try {
-//					IDE.openEditor(page, file, true);
-//				} catch (PartInitException e) {
-//				}
-//			}
-//		});
-//		monitor.worked(1);
-	}
 
 	/**
 	 * We will accept the selection in the workbench to see if
