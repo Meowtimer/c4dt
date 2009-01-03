90ba80bc54c085a5e0f9f973d923b34cdfc09b01
peterw <devnull@localhost>
2009-01-03 11:36:46 +0000

    File missing from last commit

---
+++ Clonk/src/net/arctics/clonk/resource/ResourceTester.java
@@ -0,0 +1,71 @@
+package net.arctics.clonk.resource;
+
+import net.arctics.clonk.resource.c4group.C4Group;
+
+import org.eclipse.core.expressions.PropertyTester;
+import org.eclipse.core.resources.IResource;
+import org.eclipse.ui.IFileEditorInput;
+
+public class ResourceTester extends PropertyTester {
+	
+	public boolean test(Object receiver, String property, Object[] args, Object expected) {
+		
+		// Editor? Resolve to associated resource
+		if(receiver instanceof IFileEditorInput)
+			receiver = ((IFileEditorInput) receiver).getFile();
+
+		IResource res = (IResource) receiver;
+		boolean result = false;
+		
+		// Calculate property value
+		if(property.equals("isScenario"))
+			result = isScenario(res);
+		else if(property.equals("isDefinition"))
+			result = isDefinition(res);
+		else if(property.equals("isFolder"))
+			result = isFolder(res);
+		else if(property.equals("isResource"))
+			result = isResource(res);
+		else if(property.equals("isInScenario"))
+			result = isInScenario(res);
+		else
+			assert false;
+		
+		// Compare to expected value, if given
+		if(expected == null)
+			return result;
+		else
+			return result == (Boolean) expected;
+	}
+	
+	/** @return Whether the given resource is a scenario */
+	public static boolean isScenario(IResource res) {
+		return C4Group.getGroupTypeExt(res.getFileExtension()) == C4Group.C4GroupType.ScenarioGroup;
+	}
+	
+	/** @return Whether the given resource is an object definition or object package */
+	public static boolean isDefinition(IResource res) {
+		return C4Group.getGroupTypeExt(res.getFileExtension()) == C4Group.C4GroupType.DefinitionGroup;
+	}
+
+	/** @return Whether the given resource is a scenario folder */
+	public static boolean isFolder(IResource res) {
+		return C4Group.getGroupTypeExt(res.getFileExtension()) == C4Group.C4GroupType.FolderGroup;
+	}
+
+	/** @return Whether the given resource is a resource group */
+	public static boolean isResource(IResource res) {
+		return C4Group.getGroupTypeExt(res.getFileExtension()) == C4Group.C4GroupType.ResourceGroup;
+	}
+	
+	/** @return Whether the given resource is contained in a scenario */
+	public static boolean isInScenario(IResource res) {
+		while(res.getParent() != null) {
+			if(isScenario(res))
+				return true;
+			res = res.getParent();
+		}
+		return false;
+	}
+	
+}
