3110b246fddf07a5e7248bafec5a92fbfdf3e933
Mortimer <mardmol@gmx.de>
2009-08-26 21:35:26 +0200

    Add Dependencies nature

---
+++ Clonk/src/net/arctics/clonk/resource/DependenciesProjectNature.java
@@ -0,0 +1,23 @@
+package net.arctics.clonk.resource;
+
+import org.eclipse.core.resources.IProject;
+import org.eclipse.core.resources.IProjectNature;
+import org.eclipse.core.runtime.CoreException;
+
+public class DependenciesProjectNature implements IProjectNature {
+	
+	private IProject project;
+
+	public void configure() throws CoreException {}
+
+	public void deconfigure() throws CoreException {}
+
+	public IProject getProject() {
+		return project;
+	}
+
+	public void setProject(IProject project) {
+		this.project = project;
+	}
+
+}
