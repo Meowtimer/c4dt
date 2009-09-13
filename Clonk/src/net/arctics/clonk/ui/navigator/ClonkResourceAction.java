47b24864e52441e99b6f02fb3890983c349e6f56
Mortimer <mardmol@gmx.de>
2009-09-13 18:13:24 +0200

    QuickExportAction hidden if no folder is selected

--- Clonk/src/net/arctics/clonk/ui/navigator/ClonkResourceAction.java
+++ Clonk/src/net/arctics/clonk/ui/navigator/ClonkResourceAction.java
@@ -4,12 +4,13 @@ import net.arctics.clonk.util.Utilities;
 
 import org.eclipse.core.resources.IContainer;
 import org.eclipse.jface.action.Action;
+import org.eclipse.jface.action.IAction;
 import org.eclipse.jface.viewers.ISelection;
-import org.eclipse.jface.viewers.ISelectionChangedListener;
 import org.eclipse.jface.viewers.IStructuredSelection;
-import org.eclipse.jface.viewers.SelectionChangedEvent;
+import org.eclipse.ui.IWorkbenchWindow;
+import org.eclipse.ui.IWorkbenchWindowActionDelegate;
 
-public class ClonkResourceAction extends Action implements ISelectionChangedListener {
+public class ClonkResourceAction extends Action implements IWorkbenchWindowActionDelegate {
 
 	private boolean enabled(ISelection selection) {
 		IStructuredSelection sel = (IStructuredSelection) selection;
@@ -18,15 +19,28 @@ public class ClonkResourceAction extends Action implements ISelectionChangedList
 	
 	public ClonkResourceAction() {
 		super();
-		Utilities.getProjectExplorer().getCommonViewer().addSelectionChangedListener(this);
+		//Utilities.getProjectExplorer().getCommonViewer().addSelectionChangedListener(this);
 	}
 	
 	public ClonkResourceAction(String text) {
 		super (text);
 	}
 
-	public void selectionChanged(SelectionChangedEvent event) {
-		setEnabled(enabled(event.getSelection()));
+	@Override
+	public void dispose() {
+	}
+
+	@Override
+	public void init(IWorkbenchWindow window) {
+	}
+
+	@Override
+	public void run(IAction action) {
+	}
+
+	@Override
+	public void selectionChanged(IAction action, ISelection selection) {
+		setEnabled(enabled(selection));
 	}
 
 }
--- Clonk/src/net/arctics/clonk/ui/navigator/QuickExportAction.java
+++ Clonk/src/net/arctics/clonk/ui/navigator/QuickExportAction.java
@@ -26,112 +26,97 @@ import org.eclipse.jface.viewers.TreeSelection;
 import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Event;
 import org.eclipse.ui.IWorkbench;
-import org.eclipse.ui.IWorkbenchWindow;
-import org.eclipse.ui.IWorkbenchWindowActionDelegate;
 import org.eclipse.ui.PlatformUI;
 import org.eclipse.ui.progress.IProgressService;
 
-public class QuickExportAction extends ClonkResourceAction implements IHandler, IWorkbenchWindowActionDelegate {
-		
-		public QuickExportAction() {
-			super();
-		}
-	
-		public QuickExportAction(String text) {
-			super(text);
-		}
-		
-		@SuppressWarnings("unchecked")
-		@Override
-		public void runWithEvent(Event e) {
-			super.run();
-			if (PlatformUI.getWorkbench() == null ||
+public class QuickExportAction extends ClonkResourceAction implements IHandler {
+
+	public QuickExportAction() {
+		super();
+	}
+
+	public QuickExportAction(String text) {
+		super(text);
+	}
+
+	@SuppressWarnings("unchecked")
+	@Override
+	public void runWithEvent(Event e) {
+		super.run();
+		if (PlatformUI.getWorkbench() == null ||
 				PlatformUI.getWorkbench().getActiveWorkbenchWindow() == null ||
 				PlatformUI.getWorkbench().getActiveWorkbenchWindow().getSelectionService() == null ||
 				PlatformUI.getWorkbench().getActiveWorkbenchWindow().getSelectionService().getSelection() == null)
-				return;
-			final ISelection selection = PlatformUI.getWorkbench().getActiveWorkbenchWindow().getSelectionService().getSelection();
-			if (selection != null && selection instanceof TreeSelection) {
-				IWorkbench wb = PlatformUI.getWorkbench();
-				IProgressService ps = wb.getProgressService();
-				try {
-					TreeSelection tree = (TreeSelection) selection;					
-					String c4groupPath = Utilities.getPreference(PreferenceConstants.C4GROUP_EXECUTABLE, "", null);
-					String gamePath = Utilities.getPreference(PreferenceConstants.GAME_PATH, null, null);
-					Iterator it = tree.iterator();
-					while (it.hasNext()) {
-						Object obj = it.next();
-						List<IContainer> selectedContainers = null;
-						if (obj instanceof IProject) {
-							try {
-								IResource[] selectedResources = ((IProject)obj).members(IContainer.EXCLUDE_DERIVED);
-								selectedContainers = new ArrayList<IContainer>();
-								for(int i = 0; i < selectedResources.length;i++) {
-									if (selectedResources[i] instanceof IContainer && !selectedResources[i].getName().startsWith("."))
-										selectedContainers.add((IContainer) selectedResources[i]);
-								}
-							}
-							catch (CoreException ex) {
-								ex.printStackTrace();
+			return;
+		final ISelection selection = PlatformUI.getWorkbench().getActiveWorkbenchWindow().getSelectionService().getSelection();
+		if (selection != null && selection instanceof TreeSelection) {
+			IWorkbench wb = PlatformUI.getWorkbench();
+			IProgressService ps = wb.getProgressService();
+			try {
+				TreeSelection tree = (TreeSelection) selection;					
+				String c4groupPath = Utilities.getPreference(PreferenceConstants.C4GROUP_EXECUTABLE, "", null);
+				String gamePath = Utilities.getPreference(PreferenceConstants.GAME_PATH, null, null);
+				Iterator it = tree.iterator();
+				while (it.hasNext()) {
+					Object obj = it.next();
+					List<IContainer> selectedContainers = null;
+					if (obj instanceof IProject) {
+						try {
+							IResource[] selectedResources = ((IProject)obj).members(IContainer.EXCLUDE_DERIVED);
+							selectedContainers = new ArrayList<IContainer>();
+							for(int i = 0; i < selectedResources.length;i++) {
+								if (selectedResources[i] instanceof IContainer && !selectedResources[i].getName().startsWith("."))
+									selectedContainers.add((IContainer) selectedResources[i]);
 							}
 						}
-						else if (obj instanceof IFolder) {
-							selectedContainers = new ArrayList<IContainer>(1);
-							selectedContainers.add((IContainer) obj);
-						}
-						if (selectedContainers != null) {
-							final C4GroupExporter exporter = new C4GroupExporter(selectedContainers.toArray(new IContainer[selectedContainers.size()]), c4groupPath, gamePath);
-							if (exporter.selectDestPaths())
-								ps.busyCursorWhile(new IRunnableWithProgress() {
-									public void run(IProgressMonitor pm) {
-										exporter.export(pm);
-									}
-								});
-
+						catch (CoreException ex) {
+							ex.printStackTrace();
 						}
 					}
-				} catch (Exception exc) {
-					exc.printStackTrace();
+					else if (obj instanceof IFolder) {
+						selectedContainers = new ArrayList<IContainer>(1);
+						selectedContainers.add((IContainer) obj);
+					}
+					if (selectedContainers != null) {
+						final C4GroupExporter exporter = new C4GroupExporter(selectedContainers.toArray(new IContainer[selectedContainers.size()]), c4groupPath, gamePath);
+						if (exporter.selectDestPaths())
+							ps.busyCursorWhile(new IRunnableWithProgress() {
+								public void run(IProgressMonitor pm) {
+									exporter.export(pm);
+								}
+							});
+
+					}
 				}
+			} catch (Exception exc) {
+				exc.printStackTrace();
 			}
-
-//				return true;
 		}
 
-		public void addHandlerListener(IHandlerListener handlerListener) {
-			// TODO Auto-generated method stub
-			
-		}
-
-		public void dispose() {
-			// TODO Auto-generated method stub
-			
-		}
-
-		public Object execute(ExecutionEvent event) throws ExecutionException {
-			Display.getDefault().asyncExec(new Runnable() {
-				public void run() {
-					runWithEvent(null);
-				}
-			});
-			return null;
-		}
-
-		public void removeHandlerListener(IHandlerListener handlerListener) {
-			// TODO Auto-generated method stub
-			
-		}
-
-		public void init(IWorkbenchWindow window) {
-			// TODO Auto-generated method stub
-			
-		}
-
-		public void run(IAction action) {
-			runWithEvent(null);
-		}
-
-		public void selectionChanged(IAction action, ISelection selection) {
-			//System.out.println(selection);
-		}
+		//				return true;
 	}
+
+	public void addHandlerListener(IHandlerListener handlerListener) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public Object execute(ExecutionEvent event) throws ExecutionException {
+		Display.getDefault().asyncExec(new Runnable() {
+			public void run() {
+				runWithEvent(null);
+			}
+		});
+		return null;
+	}
+
+	public void removeHandlerListener(IHandlerListener handlerListener) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public void run(IAction action) {
+		runWithEvent(null);
+	}
+
+}
