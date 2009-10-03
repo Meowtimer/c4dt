d9ea87275c10f904783030898c9c64b2fc3a4126
Mortimer <mardmol@gmx.de>
2009-10-03 02:55:46 +0200

    Remove OSX c4group export hax code so it will never be seen again

--- Clonk/src/net/arctics/clonk/resource/c4group/C4GroupExporter.java
+++ Clonk/src/net/arctics/clonk/resource/c4group/C4GroupExporter.java
@@ -22,20 +22,18 @@ import org.eclipse.ui.console.MessageConsole;
 import org.eclipse.ui.console.MessageConsoleStream;
 
 public class C4GroupExporter {
-	
+
 	private IContainer[] packs;
 	private String[] destPaths;
 	private String c4groupPath;
 	private String destinationPath;
-	
-	//private static File scratchExecFile;
-	
+
 	public C4GroupExporter(IContainer[] packs, String c4groupPath, String destinatinoPath) {
 		this.packs = packs;
 		this.c4groupPath = c4groupPath;
 		this.destinationPath = destinatinoPath;
 	}
-	
+
 	public synchronized boolean selectDestPaths() {
 		destPaths = new String[packs.length];
 		int i = -1;
@@ -65,7 +63,7 @@ public class C4GroupExporter {
 		}
 		return destPaths != null;
 	}
-	
+
 	public void export(IProgressMonitor monitor) {
 		if (monitor != null)
 			monitor.beginTask("Exporting", packs.length);
@@ -78,7 +76,6 @@ public class C4GroupExporter {
 				continue;
 			IContainer toExport = (IContainer)pack;
 			try {
-				//String OS = System.getProperty("os.name");
 				if (monitor != null)
 					monitor.subTask(toExport.getName());
 				String packPath = destPaths[i];
@@ -86,61 +83,44 @@ public class C4GroupExporter {
 				// ugh, deleting files is ugly but there seems to be no java method for putting files to trash -.-
 				if (oldFile.exists())
 					oldFile.delete();
+				// copy directory to destination and pack it in-place
 				FileOperations.copyDirectory(new File(packs[i].getRawLocation().toOSString()), oldFile);
-				/*if (OS.equals("Mac OS X")) {
-					// ugly hack :S create temporary file that uses /bin/sh to execute c4group - don't really know why it's necessary but c4group always got confused about the current directory
-					// create and make executable
-					scratchExecFile = File.createTempFile("c4groupproxy", "eclipse");
-					try {
-						Runtime.getRuntime().exec(new String[] {"/bin/chmod", "+x", scratchExecFile.getAbsolutePath()}).waitFor();
-						// write command
-						Writer writer = new OutputStreamWriter(new FileOutputStream(scratchExecFile));
-						writer.write("sh -c \"" + c4groupPath + " $1 /r -a $2/*\"");
-						writer.close();
-						// exec file with destination and source as parameters
-						Runtime.getRuntime().exec(new String[] {scratchExecFile.getAbsolutePath(), packPath, toExport.getLocation().toOSString()}).waitFor();
-					} finally {
-						scratchExecFile.delete();
-						scratchExecFile = null;
+
+				MessageConsoleStream out = null;
+				if (showExportLog) {
+					// get console
+					MessageConsole myConsole = Utilities.getClonkConsole();
+					out = myConsole.newMessageStream();
+					Utilities.displayClonkConsole();
+				}
+
+				// create c4group command line
+				String[] cmdArray = new String[] { c4groupPath, packPath, "-p" };
+				if (showExportLog) {
+					// show command line in console
+					StringBuilder cmdLine = new StringBuilder();
+					cmdLine.append("Command:");
+					for (int _i = 0; _i < cmdArray.length; _i++) {
+						String cmdE = cmdArray[_i];
+						cmdLine.append(" " + cmdE);
+					}
+					out.println(cmdLine.toString());
+				}
+
+				// run c4group
+				Process c4group = Runtime.getRuntime().exec(cmdArray, null, oldFile.getParentFile());
+				if (showExportLog) {
+					// pipe output to console
+					java.io.InputStream stream = c4group.getInputStream();
+					int read = 0;
+					byte[] buffer = new byte[256];
+					c4group.waitFor();
+
+					while((read = stream.read(buffer, 0, 256)) > 0) {
+						out.write(buffer, 0, read);
 					}
 				}
-				else*/ {
-					MessageConsoleStream out = null;
-					if (showExportLog) {
-						// get console
-						MessageConsole myConsole = Utilities.getClonkConsole();
-						out = myConsole.newMessageStream();
-						Utilities.displayClonkConsole();
-					}
-					
-					// create c4group command line
-					String[] cmdArray = new String[] { c4groupPath, packPath, "-p" };
-					if (showExportLog) {
-						// show command line in console
-						StringBuilder cmdLine = new StringBuilder();
-						cmdLine.append("Command:");
-						for (int _i = 0; _i < cmdArray.length; _i++) {
-							String cmdE = cmdArray[_i];
-							cmdLine.append(" " + cmdE);
-						}
-						out.println(cmdLine.toString());
-					}
-					
-					// run c4group
-					Process c4group = Runtime.getRuntime().exec(cmdArray, null, oldFile.getParentFile());
-					if (showExportLog) {
-						// pipe output to console
-						java.io.InputStream stream = c4group.getInputStream();
-						int read = 0;
-						byte[] buffer = new byte[256];
-						c4group.waitFor();
-						
-						while((read = stream.read(buffer, 0, 256)) > 0) {
-							out.write(buffer, 0, read);
-						}
-					}
-					oldFile = null;
-				}
+				oldFile = null;
 			} catch (IOException e1) {
 				e1.printStackTrace();
 			} catch (InterruptedException e) {
@@ -148,9 +128,10 @@ public class C4GroupExporter {
 			}
 			if (monitor != null)
 				monitor.worked(1);
+
+			if (monitor != null)
+				monitor.done();
 		}
-		if (monitor != null)
-			monitor.done();
 	}
 
 }
