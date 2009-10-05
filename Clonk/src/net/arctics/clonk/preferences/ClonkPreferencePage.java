beaf6987b9e7807f6be9020dcb96c8e7742ed53d
Mortimer <mardmol@gmx.de>
2009-10-05 14:20:57 +0200

    Version: 1.5.0.5

--- Clonk/src/net/arctics/clonk/preferences/ClonkPreferencePage.java
+++ Clonk/src/net/arctics/clonk/preferences/ClonkPreferencePage.java
@@ -16,17 +16,7 @@ import org.eclipse.ui.IWorkbench;
 import org.eclipse.ui.IWorkbenchPreferencePage;
 
 /**
- * This class represents a preference page that
- * is contributed to the Preferences dialog. By 
- * subclassing <samp>FieldEditorPreferencePage</samp>, we
- * can use the field support built into JFace that allows
- * us to create a page that is small and knows how to 
- * save, restore and apply itself.
- * <p>
- * This page is used to modify preferences only. They
- * are stored in the preference store that belongs to
- * the main plug-in class. That way, preferences can
- * be accessed directly via the preference store.
+ * Clonk preferences
  */
 public class ClonkPreferencePage extends FieldEditorPreferencePage implements IWorkbenchPreferencePage {
 
@@ -37,13 +27,7 @@ public class ClonkPreferencePage extends FieldEditorPreferencePage implements IW
 		setPreferenceStore(ClonkCore.getDefault().getPreferenceStore());
 		setDescription("Global Clonk specific preferences");
 	}
-	
-	/**
-	 * Creates the field editors. Field editors are abstractions of
-	 * the common GUI blocks needed to manipulate various types
-	 * of preferences. Each field editor knows how to save and
-	 * restore itself.
-	 */
+
 	public void createFieldEditors() {
 		
 		// FIXME: not the best place to set that
