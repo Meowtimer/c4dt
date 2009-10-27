2f32241c1e59340c226d95cb9a488cbe794cd1e4
Mortimer <mardmol@gmx.de>
2009-10-27 13:30:21 +0100

    Function entry: Complain with c4script marker type

--- Clonk/src/net/arctics/clonk/parser/inireader/Function.java
+++ Clonk/src/net/arctics/clonk/parser/inireader/Function.java
@@ -1,5 +1,6 @@
 package net.arctics.clonk.parser.inireader;
 
+import net.arctics.clonk.ClonkCore;
 import net.arctics.clonk.index.C4Object;
 import net.arctics.clonk.index.C4ObjectIntern;
 import net.arctics.clonk.parser.ParserErrorCode;
@@ -7,7 +8,7 @@ import net.arctics.clonk.parser.ParserErrorCode;
 import org.eclipse.core.resources.IFile;
 import org.eclipse.core.resources.IMarker;
 
-public class Function extends NamedReference implements IComplainingIniEntry {
+public class Function extends NamedReference implements IComplainingIniEntryValue {
 
 	@Override
 	public void complain(ComplexIniEntry context) {
@@ -16,7 +17,7 @@ public class Function extends NamedReference implements IComplainingIniEntry {
 		if (f != null) {
 			C4Object obj = C4ObjectIntern.objectCorrespondingTo(f.getParent());
 			if (obj != null && obj.findLocalFunction(this.toString(), true) == null)
-				iniUnit.markerAtValue(ParserErrorCode.UndeclaredIdentifier, context, IMarker.SEVERITY_ERROR, toString());
+				iniUnit.markerAtValue(ClonkCore.MARKER_C4SCRIPT_ERROR, ParserErrorCode.UndeclaredIdentifier, context, IMarker.SEVERITY_ERROR, toString());
 		}
 	}
 
