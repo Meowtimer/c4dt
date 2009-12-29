6d2b9b2a8750ed25532e090a257acf9b87a9a26d
Mortimer <mardmol@gmx.de>
2009-12-29 15:45:45 +0100

    ClonkPreviewView: Show image files when directly selecting them

--- C4DT/src/net/arctics/clonk/ui/navigator/ClonkPreviewView.java
+++ C4DT/src/net/arctics/clonk/ui/navigator/ClonkPreviewView.java
@@ -12,34 +12,28 @@ import org.eclipse.jface.viewers.ISelectionChangedListener;
 import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.jface.viewers.SelectionChangedEvent;
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.browser.Browser;
 import org.eclipse.swt.events.PaintEvent;
 import org.eclipse.swt.events.PaintListener;
 import org.eclipse.swt.graphics.GC;
 import org.eclipse.swt.graphics.Image;
+import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Canvas;
 import org.eclipse.swt.widgets.Composite;
+import org.eclipse.ui.IViewSite;
+import org.eclipse.ui.PartInitException;
 import org.eclipse.ui.part.*;
 
 
 /**
- * This sample class demonstrates how to plug-in a new
- * workbench view. The view shows data obtained from the
- * model. The sample creates a dummy model on the fly,
- * but a real implementation would connect to the model
- * available either in this or another plug-in (e.g. the workspace).
- * The view is connected to the model using a content provider.
- * <p>
- * The view uses a label provider to define how model
- * objects should be presented in the view. Each
- * view can present the same model objects using
- * different labels and icons, if needed. Alternatively,
- * a single label provider can be shared between views
- * in order to ensure that objects of the same type are
- * presented in the same way everywhere.
- * <p>
+ * View to show preview of files
+ * @author madeen
+ *
  */
 public class ClonkPreviewView extends ViewPart implements ISelectionChangedListener {
 
+	public static final String ID = "net.arctics.clonk.views.ClonkPreviewView";
+	
 	private final class ImageCanvas extends Canvas {
 		private ImageCanvas(Composite parent, int style) {
 			super(parent, style);
@@ -61,10 +55,9 @@ public class ClonkPreviewView extends ViewPart implements ISelectionChangedListe
 			});
 		}
 	}
-
-	public static final String ID = "net.arctics.clonk.views.ClonkPreviewView";
 	
 	private Canvas canvas;
+	private Browser browser;
 	private Image image;
 
 	public ClonkPreviewView() {
@@ -72,8 +65,15 @@ public class ClonkPreviewView extends ViewPart implements ISelectionChangedListe
 	
 	@Override
 	public void createPartControl(Composite parent) {
-		Utilities.getProjectExplorer().getCommonViewer().addSelectionChangedListener(this);
+		//parent.setLayout(new GridLayout(1, false));
 		canvas = new ImageCanvas(parent, SWT.NO_SCROLL);
+		//browser = new Browser(parent, SWT.DEFAULT);
+	}
+	
+	@Override
+	public void init(IViewSite site) throws PartInitException {
+		super.init(site);
+		Utilities.getProjectExplorer().getCommonViewer().addSelectionChangedListener(this);
 	}
 
 	@Override
@@ -86,7 +86,14 @@ public class ClonkPreviewView extends ViewPart implements ISelectionChangedListe
 		if (event.getSelection() instanceof IStructuredSelection) try {
 			IStructuredSelection structSel = (IStructuredSelection) event.getSelection();
 			Object sel = structSel.getFirstElement();
-			if (sel instanceof IContainer) {
+			if (sel instanceof IFile) {
+				IFile file = (IFile) sel;
+				String fileName = file.getName().toLowerCase();
+				if (fileName.endsWith(".png") || fileName.endsWith(".bmp")) {
+					newImage = new Image(canvas.getDisplay(), file.getLocation().toOSString());
+				}
+			}
+			else if (sel instanceof IContainer) {
 				IContainer container = (IContainer) sel;
 
 				// Title.png
