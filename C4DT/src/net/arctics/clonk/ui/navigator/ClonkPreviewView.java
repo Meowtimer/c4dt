87406511d84df78b194e220eb5b60b1881561320
Mortimer <mardmol@gmx.de>
2009-12-30 03:01:54 +0100

    ClonkPreviewPage: Display rtf files

--- C4DT/src/net/arctics/clonk/ui/navigator/ClonkPreviewView.java
+++ C4DT/src/net/arctics/clonk/ui/navigator/ClonkPreviewView.java
@@ -1,8 +1,21 @@
 package net.arctics.clonk.ui.navigator;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.StringReader;
+import java.io.StringWriter;
+
+import javax.swing.text.BadLocationException;
+import javax.swing.text.StyledDocument;
+import javax.swing.text.html.HTMLEditorKit;
+import javax.swing.text.rtf.RTFEditorKit;
+
+import net.arctics.clonk.ClonkCore;
+import net.arctics.clonk.parser.C4Structure;
 import net.arctics.clonk.parser.inireader.DefCoreUnit;
 import net.arctics.clonk.parser.inireader.IniEntry;
 import net.arctics.clonk.parser.inireader.IntegerArray;
+import net.arctics.clonk.preferences.ClonkPreferences;
 import net.arctics.clonk.util.Utilities;
 
 import org.eclipse.core.resources.IContainer;
@@ -17,6 +30,7 @@ import org.eclipse.swt.events.PaintEvent;
 import org.eclipse.swt.events.PaintListener;
 import org.eclipse.swt.graphics.GC;
 import org.eclipse.swt.graphics.Image;
+import org.eclipse.swt.layout.FillLayout;
 import org.eclipse.swt.widgets.Canvas;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.ui.IViewSite;
@@ -32,7 +46,7 @@ import org.eclipse.ui.part.*;
  */
 public class ClonkPreviewView extends ViewPart implements ISelectionChangedListener {
 
-	public static final String ID = "net.arctics.clonk.views.ClonkPreviewView";
+	public static final String ID = ClonkCore.id("views.ClonkPreviewView"); //$NON-NLS-1$
 	
 	private final class ImageCanvas extends Canvas {
 		private ImageCanvas(Composite parent, int style) {
@@ -65,9 +79,9 @@ public class ClonkPreviewView extends ViewPart implements ISelectionChangedListe
 	
 	@Override
 	public void createPartControl(Composite parent) {
-		//parent.setLayout(new GridLayout(1, false));
+		parent.setLayout(new FillLayout(SWT.VERTICAL));
 		canvas = new ImageCanvas(parent, SWT.NO_SCROLL);
-		//browser = new Browser(parent, SWT.DEFAULT);
+		browser = new Browser(parent, SWT.NONE);
 	}
 	
 	@Override
@@ -83,22 +97,42 @@ public class ClonkPreviewView extends ViewPart implements ISelectionChangedListe
 	@Override
 	public void selectionChanged(SelectionChangedEvent event) {
 		Image newImage = null;
+		String newHtml = ""; //$NON-NLS-1$
 		if (event.getSelection() instanceof IStructuredSelection) try {
 			IStructuredSelection structSel = (IStructuredSelection) event.getSelection();
 			Object sel = structSel.getFirstElement();
+			if (sel instanceof IFile && (C4Structure.pinned((IFile)sel, false, false) != null || Utilities.getScriptForFile((IFile) sel) != null))
+				sel = ((IFile)sel).getParent();
 			if (sel instanceof IFile) {
 				IFile file = (IFile) sel;
 				String fileName = file.getName().toLowerCase();
-				if (fileName.endsWith(".png") || fileName.endsWith(".bmp")) {
+				if (fileName.endsWith(".png") || fileName.endsWith(".bmp")) { //$NON-NLS-1$ //$NON-NLS-2$
 					newImage = new Image(canvas.getDisplay(), file.getLocation().toOSString());
+				}
+				else if (fileName.endsWith(".rtf")) { //$NON-NLS-1$
+					newHtml = rtfToHtml(Utilities.stringFromFile(file));
+				}
+				else if (fileName.endsWith(".txt")) { //$NON-NLS-1$
+					newHtml = Utilities.stringFromFile(file);
 				}
 			}
 			else if (sel instanceof IContainer) {
 				IContainer container = (IContainer) sel;
 
+				IResource descFile = Utilities.findMemberCaseInsensitively(container, "Desc"+ClonkPreferences.getLanguagePref()+".rtf"); //$NON-NLS-1$ //$NON-NLS-2$
+				if (descFile instanceof IFile) {
+					newHtml = rtfToHtml(Utilities.stringFromFile((IFile) descFile));
+				}
+				else {
+					descFile = Utilities.findMemberCaseInsensitively(container, "Desc"+ClonkPreferences.getLanguagePref()+".txt"); //$NON-NLS-1$ //$NON-NLS-2$
+					if (descFile instanceof IFile) {
+						newHtml = Utilities.stringFromFile((IFile) descFile);
+					}
+				}
+				
 				// Title.png
 				if (newImage == null) {
-					IResource graphicsFile = container.findMember("Title.png");
+					IResource graphicsFile = container.findMember("Title.png"); //$NON-NLS-1$
 					if (graphicsFile != null) {
 						newImage = new Image(canvas.getDisplay(), graphicsFile.getLocation().toOSString());
 					}
@@ -106,16 +140,16 @@ public class ClonkPreviewView extends ViewPart implements ISelectionChangedListe
 				
 				// part of Graphics.png as specified by DefCore.Picture
 				if (newImage == null) {
-					IResource graphicsFile = container.findMember("Graphics.png");
+					IResource graphicsFile = container.findMember("Graphics.png"); //$NON-NLS-1$
 					if (graphicsFile == null)
-						graphicsFile = container.findMember("Graphics.bmp");
+						graphicsFile = container.findMember("Graphics.bmp"); //$NON-NLS-1$
 					if (graphicsFile != null) {
 						Image fullGraphics = new Image(canvas.getDisplay(), graphicsFile.getLocation().toOSString());
 						try {
-							IResource defCoreFile = container.findMember("DefCore.txt");
+							IResource defCoreFile = container.findMember("DefCore.txt"); //$NON-NLS-1$
 							if (defCoreFile instanceof IFile) {
 								DefCoreUnit defCore = (DefCoreUnit) DefCoreUnit.pinned((IFile) defCoreFile, true, false);
-								IniEntry pictureEntry = defCore.entryInSection("DefCore", "Picture");
+								IniEntry pictureEntry = defCore.entryInSection("DefCore", "Picture"); //$NON-NLS-1$ //$NON-NLS-2$
 								if (pictureEntry != null && pictureEntry.getValueObject() instanceof IntegerArray) {
 									IntegerArray values = (IntegerArray) pictureEntry.getValueObject();
 									newImage = new Image(canvas.getDisplay(), values.get(2), values.get(3));
@@ -144,13 +178,32 @@ public class ClonkPreviewView extends ViewPart implements ISelectionChangedListe
 			image.dispose();
 		image = newImage;
 		canvas.redraw();
+		browser.setText(newHtml);
 
 	}
+
+	public String rtfToHtml(String rtf) throws IOException, BadLocationException {
+		BufferedReader input = new BufferedReader(new StringReader(rtf));
+
+		RTFEditorKit rtfKit = new RTFEditorKit();
+		StyledDocument doc = (StyledDocument) rtfKit.createDefaultDocument();
+		rtfKit.read( input, doc, 0 );
+		input.close();
+
+		HTMLEditorKit htmlKit = new HTMLEditorKit();       
+		StringWriter output = new StringWriter();
+		htmlKit.write(output, doc, 0, doc.getLength());
+
+		return output.toString();
+	}
+
 	
 	@Override
 	public void dispose() {
-		if (image != null)
+		if (image != null) {
 			image.dispose();
+			image = null;
+		}
 		CommonNavigator nav = Utilities.getProjectExplorer();
 		if (nav != null)
 			nav.getCommonViewer().removeSelectionChangedListener(this);
