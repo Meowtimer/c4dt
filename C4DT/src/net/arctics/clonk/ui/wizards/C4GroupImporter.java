fec80592dede84418462e7e485bf2c42b55cbf73
Mortimer <mardmol@gmx.de>
2010-01-05 03:37:09 +0100

    Localization fix

--- C4DT/src/net/arctics/clonk/ui/wizards/C4GroupImporter.java
+++ C4DT/src/net/arctics/clonk/ui/wizards/C4GroupImporter.java
@@ -4,7 +4,6 @@ import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.lang.reflect.InvocationTargetException;
-import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.List;
 
