da0305a9693e7e1afc460fe48712acb8c7f3fae1
Mortimer <mardmol@gmx.de>
2010-04-08 16:46:58 +0200

    C4Directive: Don't fail validation if appendto an appending to all (*) + also fail validation when not having arguments as #include or #appendto

--- C4DT/src/net/arctics/clonk/parser/c4script/C4Directive.java
+++ C4DT/src/net/arctics/clonk/parser/c4script/C4Directive.java
@@ -10,56 +10,56 @@ import net.arctics.clonk.parser.ParsingException;
 import net.arctics.clonk.parser.c4script.C4ScriptExprTree.ExprElm;
 
 public class C4Directive extends C4Declaration implements Serializable {
-	
+
 	private static final long serialVersionUID = 1L;
-	
+
 	public enum C4DirectiveType {
 		STRICT,
 		INCLUDE,
 		APPENDTO;
-		
+
 		private String lowerCase = name().toLowerCase();
-		
+
 		public static C4DirectiveType makeType(String arg) {
 			for (C4DirectiveType d : values())
 				if (d.toString().equals(arg))
 					return d;
 			return null;
 		}
-		
+
 		@Override
 		public String toString() {
 			return lowerCase;
 		}
 	}
-	
+
 	private C4DirectiveType type;
 	private String content;
 	private transient C4ID cachedID;
-	
+
 	public C4Directive(C4DirectiveType type, String content) {
 		this.content = content;
 		this.type = type;
 	}
-	
+
 	public C4Directive(String type, String content) {
 		this(C4DirectiveType.makeType(type),content);
 	}
-	
+
 	/**
 	 * @return the type
 	 */
 	public C4DirectiveType getType() {
 		return type;
 	}
-	
+
 	/**
 	 * @return the content
 	 */
 	public String getContent() {
 		return content;
 	}
-	
+
 	@Override
 	public String toString() {
 		if (content != "" && content != null) //$NON-NLS-1$
@@ -67,6 +67,11 @@ public class C4Directive extends C4Declaration implements Serializable {
 		return "#" + type.toString(); //$NON-NLS-1$
 	}
 	
+	@Override
+	public String getName() {
+		return type.toString();
+	}
+
 	public ExprElm getExprElm() {
 		return new ExprElm() {
 			@Override
@@ -79,7 +84,7 @@ public class C4Directive extends C4Declaration implements Serializable {
 			}
 		};
 	}
-	
+
 	public C4ID contentAsID() {
 		if (cachedID == null)
 			cachedID = C4ID.getID(this.getContent());
@@ -92,16 +97,23 @@ public class C4Directive extends C4Declaration implements Serializable {
 			result[d.ordinal()] = d.toString();
 		return result;
 	}
-	
+
 	public void validate(C4ScriptParser parser) throws ParsingException {
 		switch (getType()) {
-		case INCLUDE: case APPENDTO:
-			C4ID id = contentAsID();
-			C4Object obj = parser.getContainer().getIndex().getObjectNearestTo(parser.getContainer().getResource(), id);
-			if (obj == null)
-				parser.errorWithCode(ParserErrorCode.UndeclaredIdentifier, getLocation(), true, getContent());
+		case APPENDTO:
+			if (getContent() != null && getContent().equals("*"))
+				break;
+		case INCLUDE:
+			if (getContent() == null)
+				parser.errorWithCode(ParserErrorCode.MissingDirectiveArgs, getLocation(), this.toString());
+			else {
+				C4ID id = contentAsID();
+				C4Object obj = parser.getContainer().getIndex().getObjectNearestTo(parser.getContainer().getResource(), id);
+				if (obj == null)
+					parser.errorWithCode(ParserErrorCode.UndeclaredIdentifier, getLocation(), true, getContent());
+			}
 			break;
 		}
 	}
-	
+
 }
