9afafeb1bce74b46235b5eec4e775710e7b15f9a
Mortimer <mardmol@gmx.de>
2010-05-22 02:27:51 +0200

    Rename InputStreamRespecting..yadda to ClonkIndexStream and also let it not duplicate type sets

--- C4DT/src/net/arctics/clonk/resource/ClonkIndexStream.java
+++ C4DT/src/net/arctics/clonk/resource/ClonkIndexStream.java
@@ -8,16 +8,16 @@ import java.io.InputStream;
 import java.io.ObjectInputStream;
 
 import net.arctics.clonk.parser.C4ID;
+import net.arctics.clonk.parser.c4script.C4TypeSet;
 
 /**
- * Enforces that there is only one instance of C4ID with the same value
+ * Enforces that some objects won't be duplicated, like ids and such
  * @author madeen
  *
  */
-public class InputStreamRespectingUniqueIDs extends ObjectInputStream {
+public class ClonkIndexStream extends ObjectInputStream {
 
-	public InputStreamRespectingUniqueIDs(InputStream input)
-			throws IOException {
+	public ClonkIndexStream(InputStream input) throws IOException {
 		super(input);
 		enableResolveObject(true);
 	}
@@ -26,8 +26,10 @@ public class InputStreamRespectingUniqueIDs extends ObjectInputStream {
 	protected Object resolveObject(Object obj) throws IOException {
 		if (obj.getClass() == C4ID.class) {
 			//System.out.println(obj.toString());
-			return ((C4ID)obj).makeSpecial();
+			return ((C4ID)obj).internalize();
 		}
+		else if (obj.getClass() == C4TypeSet.class)
+			return ((C4TypeSet)obj).internalize();
 		return super.resolveObject(obj);
 	}
 	
