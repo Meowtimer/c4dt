0314bba3fbfc9a2ab4c5d9ae9c160acbc87813bd
Mortimer <mardmol@gmx.de>
2010-09-10 18:11:28 +0200

    Don't call deprecated addSaveParticipant method

--- C4DT/src/net/arctics/clonk/ClonkCore.java
+++ C4DT/src/net/arctics/clonk/ClonkCore.java
@@ -115,7 +115,6 @@ public class ClonkCore extends AbstractUIPlugin implements ISaveParticipant, IRe
 	 * (non-Javadoc)
 	 * @see org.eclipse.ui.plugin.AbstractUIPlugin#start(org.osgi.framework.BundleContext)
 	 */
-	@SuppressWarnings("deprecation")
 	public void start(BundleContext context) throws Exception {
 		super.start(context);
 		plugin = this;
@@ -123,7 +122,7 @@ public class ClonkCore extends AbstractUIPlugin implements ISaveParticipant, IRe
 		loadActiveEngine();
 
 		// FIXME: this is deprecated and stuff
-		ResourcesPlugin.getWorkspace().addSaveParticipant(this, this);
+		ResourcesPlugin.getWorkspace().addSaveParticipant(PLUGIN_ID, this);
 		//ResourcesPlugin.getWorkspace().addResourceChangeListener(this, IResourceChangeEvent.PRE_DELETE);
 
 		registerStructureClasses();
