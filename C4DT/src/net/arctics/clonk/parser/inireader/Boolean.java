05f72829ef9375b4deb97f8cdc9c8b4dc6cf91e4
Mortimer <mardmol@gmx.de>
2010-11-16 01:45:25 +0100

    Boolean ini entries: Accept 'true' and 'false'

--- C4DT/src/net/arctics/clonk/parser/inireader/Boolean.java
+++ C4DT/src/net/arctics/clonk/parser/inireader/Boolean.java
@@ -1,5 +1,8 @@
 package net.arctics.clonk.parser.inireader;
 
+import net.arctics.clonk.parser.c4script.Keywords;
+import net.arctics.clonk.parser.inireader.IniData.IniDataEntry;
+
 /**
  * Specialization to have fancy checkboxes for it in the ini editor
  */
@@ -7,5 +10,15 @@ public class Boolean extends UnsignedInteger {
 	@Override
 	public Object convertToPrimitive() {
 		return this.getNumber() != 0;
+	}
+	@Override
+	public void setInput(String input, IniDataEntry entryData, IniUnit context) throws IniParserException {
+		if (input.equals(Keywords.True)) {
+			setNumber(1);
+		} else if (input.equals(Keywords.False)) {
+			setNumber(0);
+		} else {
+			super.setInput(input, entryData, context);
+		}
 	}
 }
