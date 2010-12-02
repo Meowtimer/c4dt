a38d2d758a8388b870744268074aaa49a0c7f428
Mortimer <mardmol@gmx.de>
2010-12-02 11:12:13 +0100

    C4ScriptToCPPConverter: Type around

--- C4DT/src/net/arctics/clonk/parser/c4script/C4ScriptToCPPConverter.java
+++ C4DT/src/net/arctics/clonk/parser/c4script/C4ScriptToCPPConverter.java
@@ -1,18 +1,26 @@
 package net.arctics.clonk.parser.c4script;
 
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
-import java.io.StringWriter;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.io.Writer;
 import java.util.HashMap;
 import java.util.Map;
 
+import net.arctics.clonk.ClonkCore;
+import net.arctics.clonk.command.BodyPreservingFunction;
+import net.arctics.clonk.command.ExecutableScript;
 import net.arctics.clonk.parser.ParsingException;
 import net.arctics.clonk.parser.c4script.ast.Block;
 import net.arctics.clonk.parser.c4script.ast.ExprElm;
 import net.arctics.clonk.parser.c4script.ast.ExprWriter;
+import net.arctics.clonk.parser.c4script.ast.NumberLiteral;
 import net.arctics.clonk.parser.c4script.ast.StringLiteral;
 import net.arctics.clonk.parser.c4script.ast.VarDeclarationStatement;
 import net.arctics.clonk.parser.c4script.ast.VarDeclarationStatement.VarInitialization;
+import net.arctics.clonk.util.Utilities;
 
 public class C4ScriptToCPPConverter {
 	
@@ -43,7 +51,11 @@ public class C4ScriptToCPPConverter {
 					append(regString(((StringLiteral) elm).getLiteral()));
 					return true;
 				}
-				if (elm instanceof VarDeclarationStatement) {
+				else if (elm instanceof NumberLiteral) {
+					append(String.format("C4Value(%d)", ((NumberLiteral)elm).getLiteral()));
+					return true;
+				}
+				else if (elm instanceof VarDeclarationStatement) {
 					VarDeclarationStatement statement = (VarDeclarationStatement) elm;
 					append("C4Value"); //$NON-NLS-1$
 					append(" "); //$NON-NLS-1$
@@ -84,19 +96,48 @@ public class C4ScriptToCPPConverter {
 			}
 		}, depth);
 	}
-	public void print(C4Function function, Block body, Writer output) {
-		
+
+	public void print(C4Function function, Block body, Writer output) throws IOException {
+		output.append("static ");
+		output.append(C4Type.cppTypeFromType(function.getReturnType()));
+		output.append(" ");
+		output.append(function.getName());
+		output.append("(C4AulContext *cthr");
+		for (C4Variable parm : function.getParameters()) {
+			output.append(", ");
+			output.append(C4Type.cppTypeFromType(parm.getType()));
+			output.append(" ");
+			output.append(parm.getName());
+		}
+		output.append(")\n{\n");
+		this.print(body, output, 0); //$NON-NLS-1$
+		output.append("}\n");
 	}
 	
-	public static void test() {
-		C4ScriptToCPPConverter converter = new C4ScriptToCPPConverter();
-		try {
-			StringWriter writer;
-			converter.print(C4ScriptParser.parseStandaloneStatement("var ugh = 123, bla = \"ugh\";", null, null), writer = new StringWriter(), 0); //$NON-NLS-1$
-			System.out.println(writer.getBuffer().toString());
-		} catch (ParsingException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+	public void print(C4ScriptBase script, Writer output) throws IOException {
+		for (C4Function f : script.functions()) {
+			if (f instanceof BodyPreservingFunction) {
+				BodyPreservingFunction bf = (BodyPreservingFunction) f;
+				print(bf, bf.getBodyBlock(), output);
+			}
 		}
+	}
+	
+	public static void main(String[] args) throws IOException, ParsingException {
+		if (args.length == 0) {
+			System.out.println("Provide script file");
+			return;
+		}
+		File scriptToConvert = new File(args[0]);
+		if (!scriptToConvert.exists()) {
+			System.out.println(String.format("File %s does not exist", args[0]));
+			return;
+		}
+		ClonkCore.headlessInitialize("OpenClonk");
+		InputStreamReader reader = new InputStreamReader(new FileInputStream(scriptToConvert));
+		String script = Utilities.stringFromReader(reader);
+		ExecutableScript scriptObj = new ExecutableScript(scriptToConvert.getName(), script);
+		PrintWriter printWriter = new PrintWriter(System.out);
+		new C4ScriptToCPPConverter().print(scriptObj, printWriter);
 	}
 }
