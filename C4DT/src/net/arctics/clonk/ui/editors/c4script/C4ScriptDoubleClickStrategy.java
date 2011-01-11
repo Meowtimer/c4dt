b9355a14aa9a10ac5a347de1c06d5a2b93c9fc5a
Mortimer <mardmol@gmx.de>
2011-01-11 21:44:45 +0100

    Disable double click strategy to select expressions

--- C4DT/src/net/arctics/clonk/ui/editors/c4script/C4ScriptDoubleClickStrategy.java
+++ C4DT/src/net/arctics/clonk/ui/editors/c4script/C4ScriptDoubleClickStrategy.java
@@ -3,14 +3,6 @@ package net.arctics.clonk.ui.editors.c4script;
 import org.eclipse.jface.text.DefaultTextDoubleClickStrategy;
 import org.eclipse.jface.text.ITextViewer;
 
-import net.arctics.clonk.parser.c4script.C4Function;
-import net.arctics.clonk.parser.c4script.C4ScriptBase;
-import net.arctics.clonk.parser.c4script.C4ScriptParser;
-import net.arctics.clonk.util.Utilities;
-import net.arctics.clonk.parser.c4script.C4ScriptParser.ExpressionsAndStatementsReportingFlavour;
-import net.arctics.clonk.parser.c4script.ast.ExprElm;
-import net.arctics.clonk.parser.c4script.ast.StringLiteral;
-
 public class C4ScriptDoubleClickStrategy extends DefaultTextDoubleClickStrategy {
 	
 	private C4ScriptSourceViewerConfiguration configuration;
@@ -25,7 +17,7 @@ public class C4ScriptDoubleClickStrategy extends DefaultTextDoubleClickStrategy 
 		if (pos < 0)
 			return;
 		
-		C4ScriptBase script = Utilities.getScriptForEditor(configuration.getEditor());
+		/*C4ScriptBase script = Utilities.getScriptForEditor(configuration.getEditor());
 		C4Function func = script.funcAt(pos);
 		if (func != null) {
 			ExpressionLocator locator = new ExpressionLocator(pos-func.getBody().getOffset());
@@ -35,7 +27,7 @@ public class C4ScriptDoubleClickStrategy extends DefaultTextDoubleClickStrategy 
 				viewer.setSelectedRange(func.getBody().getOffset()+expr.getExprStart()+1, expr.getLength()-2);
 				return;
 			}
-		}
+		}*/
 		super.doubleClicked(viewer);
 	}
 }
