934fe6d75fe32a778d172b3f35fdf035796c4065
Mortimer <mardmol@gmx.de>
2011-01-13 14:37:23 +0100

    C4ScriptDoubleClickStrategy: Add minimal usefulness (selecting identifiers and blocks by double-clicking)

--- C4DT/src/net/arctics/clonk/ui/editors/c4script/C4ScriptDoubleClickStrategy.java
+++ C4DT/src/net/arctics/clonk/ui/editors/c4script/C4ScriptDoubleClickStrategy.java
@@ -1,5 +1,16 @@
 package net.arctics.clonk.ui.editors.c4script;
 
+import net.arctics.clonk.parser.c4script.C4Function;
+import net.arctics.clonk.parser.c4script.C4ScriptBase;
+import net.arctics.clonk.parser.c4script.C4ScriptParser;
+import net.arctics.clonk.parser.c4script.C4ScriptParser.ExpressionsAndStatementsReportingFlavour;
+import net.arctics.clonk.parser.c4script.ast.AccessDeclaration;
+import net.arctics.clonk.parser.c4script.ast.Block;
+import net.arctics.clonk.parser.c4script.ast.ExprElm;
+import net.arctics.clonk.parser.c4script.ast.PropListExpression;
+import net.arctics.clonk.parser.c4script.ast.StringLiteral;
+import net.arctics.clonk.util.Utilities;
+
 import org.eclipse.jface.text.DefaultTextDoubleClickStrategy;
 import org.eclipse.jface.text.ITextViewer;
 
@@ -17,7 +28,7 @@ public class C4ScriptDoubleClickStrategy extends DefaultTextDoubleClickStrategy 
 		if (pos < 0)
 			return;
 		
-		/*C4ScriptBase script = Utilities.getScriptForEditor(configuration.getEditor());
+		C4ScriptBase script = Utilities.getScriptForEditor(configuration.getEditor());
 		C4Function func = script.funcAt(pos);
 		if (func != null) {
 			ExpressionLocator locator = new ExpressionLocator(pos-func.getBody().getOffset());
@@ -26,8 +37,15 @@ public class C4ScriptDoubleClickStrategy extends DefaultTextDoubleClickStrategy 
 			if (expr instanceof StringLiteral) {
 				viewer.setSelectedRange(func.getBody().getOffset()+expr.getExprStart()+1, expr.getLength()-2);
 				return;
+			} else if (expr instanceof AccessDeclaration) {
+				AccessDeclaration accessDec = (AccessDeclaration) expr;
+				viewer.setSelectedRange(func.getBody().getOffset()+accessDec.getIdentifierStart(), accessDec.getIdentifierLength());
+				return;
+			} else if (expr instanceof PropListExpression || expr instanceof Block) {
+				viewer.setSelectedRange(expr.getExprStart()+func.getBody().getOffset(), expr.getLength());
+				return;
 			}
-		}*/
+		}
 		super.doubleClicked(viewer);
 	}
 }
