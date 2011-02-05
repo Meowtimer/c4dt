877b3f22229001bf5b9cb502b3c828df5e7e00ef
Mortimer <mardmol@gmx.de>
2011-02-05 21:02:33 +0100

    Message about 1.5.9 say "check", not "re-set" implying that it could still be right (due to guessing the engine based on the project name)

--- C4DT/src/net/arctics/clonk/Milestones.java
+++ C4DT/src/net/arctics/clonk/Milestones.java
@@ -3,5 +3,5 @@ package net.arctics.clonk;
 import org.osgi.framework.Version;
 
 public class Milestones {
-	public static final Version VERSION_THAT_INTRODUCED_PROJECT_SETTINGS = new Version("1.5.9.0");
+	public static final Version VERSION_THAT_INTRODUCED_PROJECT_SETTINGS = new Version(1, 5, 9);
 }
