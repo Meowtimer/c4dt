22c70477a0e97b26aaebe775df738afee230483f
Mortimer <mardmol@gmx.de>
2011-10-05 11:59:59 +0200

    Indentation D:

--- C4DT/src/net/arctics/clonk/preferences/EngineConfigurationPrefPage.java
+++ C4DT/src/net/arctics/clonk/preferences/EngineConfigurationPrefPage.java
@@ -39,7 +39,7 @@ import org.eclipse.ui.IWorkbenchPreferencePage;
  *
  */
 public class EngineConfigurationPrefPage extends FieldEditorPreferencePage implements IExecutableExtension, IWorkbenchPreferencePage {
-	
+
 	private String myEngine;
 	private FileFieldEditor c4GroupEditor;
 	private FileFieldEditor engineExecutableEditor;
@@ -48,7 +48,7 @@ public class EngineConfigurationPrefPage extends FieldEditorPreferencePage imple
 	private EngineConfigPrefStore engineConfigPrefStore;
 
 	private class GamePathEditor extends DirectoryFieldEditor {
-		
+
 		public GamePathEditor(Composite parent, String name, String labelText) {
 			super(name, labelText, parent);
 		}
@@ -87,11 +87,11 @@ public class EngineConfigurationPrefPage extends FieldEditorPreferencePage imple
 			//previousGamePath = gamePathText.toLowerCase();
 		}
 	}
-	
+
 	private class EngineRelatedFileFieldEditor extends FileFieldEditor {
 		// redeclare since field from super class is not accessible -.-
 		private String[] extensions;
-		
+
 		public EngineRelatedFileFieldEditor(String pref, String title, Composite parent, String[] extensions) {
 			super(pref, title, parent);
 			this.extensions = extensions;
@@ -106,7 +106,7 @@ public class EngineConfigurationPrefPage extends FieldEditorPreferencePage imple
 			return dialog.open();
 		}
 	}
-	
+
 	private class EngineExecutableEditor extends EngineRelatedFileFieldEditor {
 		public EngineExecutableEditor(String pref, String title, Composite parent, String[] extensions) {
 			super(pref, title, parent, extensions);
@@ -126,9 +126,9 @@ public class EngineConfigurationPrefPage extends FieldEditorPreferencePage imple
 				return null;
 		}
 	}
-	
+
 	private class EngineConfigPrefStore extends PreferenceStore {
-		
+
 		private Engine.EngineSettings settings = (EngineSettings) ClonkCore.getDefault().loadEngine(myEngine).getCurrentSettings().clone();
 
 		@Override
@@ -139,7 +139,7 @@ public class EngineConfigurationPrefPage extends FieldEditorPreferencePage imple
 				e1.printStackTrace();
 			}
 		}
-		
+
 		private Object val(EngineSettings settings, String attrName) {
 			try {
 				return settings.getClass().getField(attrName).get(settings);
@@ -147,7 +147,7 @@ public class EngineConfigurationPrefPage extends FieldEditorPreferencePage imple
 				return null;
 			}
 		}
-		
+
 		@Override
 		public void setValue(String name, boolean value) {
 			try {
@@ -161,7 +161,7 @@ public class EngineConfigurationPrefPage extends FieldEditorPreferencePage imple
 		public String getString(String name) {
 			return (String)val(settings, name);
 		}
-		
+
 		@Override
 		public boolean getBoolean(String name) {
 			return (Boolean)val(settings, name);
@@ -171,7 +171,7 @@ public class EngineConfigurationPrefPage extends FieldEditorPreferencePage imple
 		public String getDefaultString(String name) { 
 			return (String)val(ClonkCore.getDefault().loadEngine(myEngine).getCurrentSettings(), name);
 		}
-		
+
 		@Override
 		public boolean getDefaultBoolean(String name) {
 			return (Boolean)val(ClonkCore.getDefault().loadEngine(myEngine).getCurrentSettings(), name);
@@ -208,83 +208,83 @@ public class EngineConfigurationPrefPage extends FieldEditorPreferencePage imple
 		engineConfigurationComposite.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false, 3, 1));
 		if (!Beans.isDesignTime())
 			setPreferenceStore(engineConfigPrefStore);
-		
+
 		addField(
-				gamePathEditor = new GamePathEditor(engineConfigurationComposite, "gamePath", Messages.GamePath) //$NON-NLS-1$
-		);
+			gamePathEditor = new GamePathEditor(engineConfigurationComposite, "gamePath", Messages.GamePath) //$NON-NLS-1$
+			);
 		addField(
-				c4GroupEditor = new EngineRelatedFileFieldEditor(
-						"c4GroupPath", Messages.C4GroupExecutable, //$NON-NLS-1$
-						engineConfigurationComposite,
-						appExtensions(false)
+			c4GroupEditor = new EngineRelatedFileFieldEditor(
+				"c4GroupPath", Messages.C4GroupExecutable, //$NON-NLS-1$
+				engineConfigurationComposite,
+				appExtensions(false)
 				)
-		);
+			);
 		addField(
-				engineExecutableEditor = new EngineExecutableEditor("engineExecutablePath", Messages.EngineExecutable, //$NON-NLS-1$
-						engineConfigurationComposite, appExtensions(true))
-		);
+			engineExecutableEditor = new EngineExecutableEditor("engineExecutablePath", Messages.EngineExecutable, //$NON-NLS-1$
+				engineConfigurationComposite, appExtensions(true))
+			);
 		addField(
-				new DirectoryFieldEditor(
-						"repositoryPath", //$NON-NLS-1$
-						Messages.OpenClonkRepo,
-						engineConfigurationComposite
+			new DirectoryFieldEditor(
+				"repositoryPath", //$NON-NLS-1$
+				Messages.OpenClonkRepo,
+				engineConfigurationComposite
 				)
-		);
+			);
 		addField(
-				new StringButtonFieldEditor(
-						"docURLTemplate", //$NON-NLS-1$
-						Messages.DocumentURLTemplate,
-						engineConfigurationComposite
+			new StringButtonFieldEditor(
+				"docURLTemplate", //$NON-NLS-1$
+				Messages.DocumentURLTemplate,
+				engineConfigurationComposite
 				) {
-					private Button checker;
-					@Override
-					protected Button getChangeControl(Composite parent) {
-						if (checker == null) {
-							checker = new Button(parent, SWT.CHECK);
-							checker.setText(Messages.EngineConfigurationPrefPage_UseRepositoryDocsFolder0);
-							checker.addSelectionListener(new SelectionAdapter() {
-								@Override
-								public void widgetSelected(SelectionEvent e) {
-									setTextControlEnablement();
-								}
-							});
-						}
-						return checker;
-					};
-					@Override
-					public void setEnabled(boolean enabled, Composite parent) {
-						super.setEnabled(enabled, parent);
-						if (checker != null)
-							checker.setEnabled(enabled);
-					};
-					@Override
-					protected String changePressed() {
-						return ""; //$NON-NLS-1$
-					};
-					private void setTextControlEnablement() {
-						getTextControl().setEnabled(!checker.getSelection());
+				private Button checker;
+				@Override
+				protected Button getChangeControl(Composite parent) {
+					if (checker == null) {
+						checker = new Button(parent, SWT.CHECK);
+						checker.setText(Messages.EngineConfigurationPrefPage_UseRepositoryDocsFolder0);
+						checker.addSelectionListener(new SelectionAdapter() {
+							@Override
+							public void widgetSelected(SelectionEvent e) {
+								setTextControlEnablement();
+							}
+						});
 					}
-					@Override
-					public void load() {
-						super.load();
-						checker.setSelection(getPreferenceStore().getBoolean("useDocsFromRepository")); //$NON-NLS-1$
-						setTextControlEnablement();
-					};
-					@Override
-					protected void doStore() {
-						super.doStore();
-						getPreferenceStore().setValue("useDocsFromRepository", checker.getSelection()); //$NON-NLS-1$
-					};
+					return checker;
+				};
+				@Override
+				public void setEnabled(boolean enabled, Composite parent) {
+					super.setEnabled(enabled, parent);
+					if (checker != null)
+						checker.setEnabled(enabled);
+				};
+				@Override
+				protected String changePressed() {
+					return ""; //$NON-NLS-1$
+				};
+				private void setTextControlEnablement() {
+					getTextControl().setEnabled(!checker.getSelection());
 				}
-		);
+				@Override
+				public void load() {
+					super.load();
+					checker.setSelection(getPreferenceStore().getBoolean("useDocsFromRepository")); //$NON-NLS-1$
+					setTextControlEnablement();
+				};
+				@Override
+				protected void doStore() {
+					super.doStore();
+					getPreferenceStore().setValue("useDocsFromRepository", checker.getSelection()); //$NON-NLS-1$
+				};
+			}
+			);
 		addField(
-				new BooleanFieldEditor(
-						"readDocumentationFromRepository", //$NON-NLS-1$
-						Messages.EngineConfigurationPrefPage_ReadDocFromRepository,
-						engineConfigurationComposite
+			new BooleanFieldEditor(
+				"readDocumentationFromRepository", //$NON-NLS-1$
+				Messages.EngineConfigurationPrefPage_ReadDocFromRepository,
+				engineConfigurationComposite
 				)
-		);
-		
+			);
+
 		GridLayout gridLayout = new GridLayout();
 		gridLayout.numColumns = 3;
 		engineConfigurationComposite.setLayout(gridLayout);
@@ -295,7 +295,7 @@ public class EngineConfigurationPrefPage extends FieldEditorPreferencePage imple
 		myEngine = data.toString();
 		engineConfigPrefStore = new EngineConfigPrefStore();
 	}
-	
+
 	@Override
 	public String getTitle() {
 		return String.format(Messages.EngineConfigurationPrefPage_SettingsForEngineTitle, myEngine);
@@ -303,15 +303,15 @@ public class EngineConfigurationPrefPage extends FieldEditorPreferencePage imple
 
 	@Override
 	public void init(IWorkbench workbench) {
-		
+
 	}
-	
+
 	@Override
 	protected void performDefaults() {
 		engineConfigPrefStore.reset();
 		super.performDefaults();
 	}
-	
+
 	@Override
 	public boolean performOk() {
 		boolean result = super.performOk();
@@ -319,12 +319,12 @@ public class EngineConfigurationPrefPage extends FieldEditorPreferencePage imple
 			engineConfigPrefStore.apply();
 		return result;
 	}
-	
+
 	@Override
 	protected void addField(FieldEditor editor) {
 		super.addField(editor);
 	}
-	
+
 	@Override
 	protected void initialize() {
 		super.initialize();
@@ -333,15 +333,15 @@ public class EngineConfigurationPrefPage extends FieldEditorPreferencePage imple
 			e.load();
 		}
 	}
-	
+
 	public FileFieldEditor getC4GroupEditor() {
 		return c4GroupEditor;
 	}
-	
+
 	public FileFieldEditor getEngineExecutableEditor() {
 		return engineExecutableEditor;
 	}
-	
+
 	public DirectoryFieldEditor getGamePathEditor() {
 		return gamePathEditor;
 	}
