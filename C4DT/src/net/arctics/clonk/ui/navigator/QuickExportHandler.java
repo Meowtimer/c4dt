5d019e444e33bd2f11f3e62bc6cb4ce5054a236b
Mortimer <mardmol@gmx.de>
2012-03-11 14:56:47 +0100

    QuickExportHandler: When invoked with an active editor, find the top level folder (not the project) the file being edited is inside and export that. Also, when having selected a file in project explorer, export top level folder as well.

--- C4DT/src/net/arctics/clonk/ui/navigator/QuickExportHandler.java
+++ C4DT/src/net/arctics/clonk/ui/navigator/QuickExportHandler.java
@@ -1,14 +1,15 @@
 package net.arctics.clonk.ui.navigator;
 
 import java.util.ArrayList;
-
 import java.util.Iterator;
 import java.util.List;
 
 import net.arctics.clonk.resource.c4group.C4GroupExporter;
+
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
 import org.eclipse.core.resources.IContainer;
+import org.eclipse.core.resources.IFile;
 import org.eclipse.core.resources.IFolder;
 import org.eclipse.core.resources.IProject;
 import org.eclipse.core.resources.IResource;
@@ -17,7 +18,10 @@ import org.eclipse.jface.dialogs.ProgressMonitorDialog;
 import org.eclipse.jface.viewers.ISelection;
 import org.eclipse.jface.viewers.TreeSelection;
 import org.eclipse.swt.widgets.Display;
+import org.eclipse.ui.IWorkbenchPart;
 import org.eclipse.ui.handlers.HandlerUtil;
+import org.eclipse.ui.part.EditorPart;
+import org.eclipse.ui.part.FileEditorInput;
 
 public class QuickExportHandler extends ClonkResourceHandler {
 
@@ -27,42 +31,59 @@ public class QuickExportHandler extends ClonkResourceHandler {
 			@Override
 			@SuppressWarnings("rawtypes")
 			public void run() {
-				final ISelection selection = HandlerUtil.getCurrentSelection(event);
-				if (selection != null && selection instanceof TreeSelection) {
-					try {
-						TreeSelection tree = (TreeSelection) selection;
-						Iterator it = tree.iterator();
-						while (it.hasNext()) {
-							Object obj = it.next();
-							List<IContainer> selectedContainers = null;
-							if (obj instanceof IProject) {
-								try {
-									IResource[] selectedResources = ((IProject)obj).members(IContainer.EXCLUDE_DERIVED);
-									selectedContainers = new ArrayList<IContainer>();
-									for(int i = 0; i < selectedResources.length;i++) {
-										if (selectedResources[i] instanceof IContainer && !selectedResources[i].getName().startsWith(".")) //$NON-NLS-1$
-											selectedContainers.add((IContainer) selectedResources[i]);
+				try {
+					List<IContainer> selectedContainers = new ArrayList<IContainer>();
+
+					IWorkbenchPart part = HandlerUtil.getActivePart(event);
+					if (part instanceof EditorPart && ((EditorPart)part).getEditorInput() instanceof FileEditorInput) {
+						IFile file = ((FileEditorInput)((EditorPart)part).getEditorInput()).getFile();
+						IContainer r; 
+						for (r = file.getParent(); r != null && !(r.getParent() instanceof IProject); r = r.getParent());
+						if (r != null) {
+							selectedContainers.add(r);
+						}
+					} else {
+						final ISelection selection = HandlerUtil.getCurrentSelection(event);
+						if (selection != null && selection instanceof TreeSelection) {
+							TreeSelection tree = (TreeSelection) selection;
+							Iterator it = tree.iterator();
+							while (it.hasNext()) {
+								Object obj = it.next();
+								if (obj instanceof IProject) {
+									try {
+										IResource[] selectedResources = ((IProject)obj).members(IContainer.EXCLUDE_DERIVED);
+										for(int i = 0; i < selectedResources.length;i++) {
+											if (selectedResources[i] instanceof IContainer && !selectedResources[i].getName().startsWith(".")) //$NON-NLS-1$
+												selectedContainers.add((IContainer) selectedResources[i]);
+										}
+									}
+									catch (CoreException ex) {
+										ex.printStackTrace();
 									}
 								}
-								catch (CoreException ex) {
-									ex.printStackTrace();
+								else if (obj instanceof IFolder) {
+									selectedContainers.add((IContainer) obj);
 								}
-							}
-							else if (obj instanceof IFolder) {
-								selectedContainers = new ArrayList<IContainer>(1);
-								selectedContainers.add((IContainer) obj);
-							}
-							if (selectedContainers != null) {
-								final C4GroupExporter exporter = new C4GroupExporter(selectedContainers.toArray(new IContainer[selectedContainers.size()]), null);
-								if (exporter.selectDestPaths()) {
-									final ProgressMonitorDialog progressDialog = new ProgressMonitorDialog(HandlerUtil.getActiveWorkbenchWindow(event).getShell());
-									progressDialog.run(false, true, exporter);
+								else if (obj instanceof IFile) {
+									IFile file = (IFile)obj;
+									IContainer r;
+									for (r = file.getParent(); r != null && !(r.getParent() instanceof IProject); r = r.getParent());
+									if (r != null) {
+										selectedContainers.add(r);
+									}
 								}
 							}
 						}
-					} catch (Exception exc) {
-						exc.printStackTrace();
 					}
+					if (selectedContainers.size() > 0) {
+						final C4GroupExporter exporter = new C4GroupExporter(selectedContainers.toArray(new IContainer[selectedContainers.size()]), null);
+						if (exporter.selectDestPaths()) {
+							final ProgressMonitorDialog progressDialog = new ProgressMonitorDialog(HandlerUtil.getActiveWorkbenchWindow(event).getShell());
+							progressDialog.run(false, true, exporter);
+						}
+					}
+				} catch (Exception e) {
+					e.printStackTrace();
 				}
 			}
 		});
