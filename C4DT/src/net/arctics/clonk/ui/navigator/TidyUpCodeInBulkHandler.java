d5b9ddf44b11580a6561b483b03993430650df7b
Mortimer <mardmol@gmx.de>
2012-09-03 11:04:03 +0200

    TidyUpCodeInBulkHandler: Temporarily disable auto-build

--- C4DT/src/net/arctics/clonk/ui/navigator/TidyUpCodeInBulkHandler.java
+++ C4DT/src/net/arctics/clonk/ui/navigator/TidyUpCodeInBulkHandler.java
@@ -6,9 +6,9 @@ import java.util.LinkedList;
 import java.util.List;
 
 import net.arctics.clonk.Core;
-import net.arctics.clonk.parser.c4script.Script;
-import net.arctics.clonk.parser.c4script.C4ScriptParser;
 import net.arctics.clonk.parser.ParsingException;
+import net.arctics.clonk.parser.c4script.C4ScriptParser;
+import net.arctics.clonk.parser.c4script.Script;
 import net.arctics.clonk.ui.editors.actions.c4script.TidyUpCodeAction;
 import net.arctics.clonk.util.UI;
 import net.arctics.clonk.util.Utilities;
@@ -22,6 +22,9 @@ import org.eclipse.core.resources.IFolder;
 import org.eclipse.core.resources.IProject;
 import org.eclipse.core.resources.IResource;
 import org.eclipse.core.resources.IResourceVisitor;
+import org.eclipse.core.resources.IWorkspace;
+import org.eclipse.core.resources.IWorkspaceDescription;
+import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.jface.dialogs.ProgressMonitorDialog;
@@ -54,21 +57,18 @@ public class TidyUpCodeInBulkHandler extends AbstractHandler {
 			final List<IContainer> selectedContainers = new LinkedList<IContainer>();
 			while (it.hasNext()) {
 				Object obj = it.next();
-				if (obj instanceof IProject) {
+				if (obj instanceof IProject)
 					try {
 						IResource[] selectedResources = ((IProject)obj).members(IContainer.EXCLUDE_DERIVED);
-						for(int i = 0; i < selectedResources.length;i++) {
+						for(int i = 0; i < selectedResources.length;i++)
 							if (selectedResources[i] instanceof IContainer && !selectedResources[i].getName().startsWith(".")) //$NON-NLS-1$
 								selectedContainers.add((IContainer) selectedResources[i]);
-						}
 					}
 					catch (CoreException ex) {
 						ex.printStackTrace();
 					}
-				}
-				else if (obj instanceof IFolder) {
+				else if (obj instanceof IFolder)
 					selectedContainers.add((IContainer) obj);
-				}
 			}
 			if (selectedContainers.size() > 0) {
 				ProgressMonitorDialog progressDialog = new ProgressMonitorDialog(PlatformUI.getWorkbench().getActiveWorkbenchWindow().getShell());
@@ -87,65 +87,83 @@ public class TidyUpCodeInBulkHandler extends AbstractHandler {
 										return true;
 									}
 								};
-								for (IContainer container : selectedContainers) {
+								for (IContainer container : selectedContainers)
 									try {
 										container.accept(countingVisitor);
 									} catch (CoreException e) {
 										e.printStackTrace();
 									}
-								}
 							}
-							monitor.beginTask(Messages.TidyUpCodeInBulkAction_ConvertingCode, counter);
-							for (IContainer container : selectedContainers) {
-								try {
-									final TextFileDocumentProvider textFileDocProvider = Core.instance().getTextFileDocumentProvider();
-									final List<IFile> failedSaves = new LinkedList<IFile>();
-									container.accept(new IResourceVisitor() {
-										@Override
-										public boolean visit(IResource resource) throws CoreException {
-											if (resource instanceof IFile) {
-												IFile file = (IFile) resource;
-												Script script = Script.get(file, true);
-												if (script != null) {
-													C4ScriptParser parser = new C4ScriptParser(file, script);
-													try {
-														parser.parse();
-													} catch (ParsingException e1) {
-														e1.printStackTrace();
-													}
-													textFileDocProvider.connect(file);
-													try {
-														IDocument document = textFileDocProvider.getDocument(file);
-
-														if (document != null)
-															TidyUpCodeAction.runOnDocument(script, null, parser, document);
-
-														try {
-															textFileDocProvider.setEncoding(document, textFileDocProvider.getDefaultEncoding());
-															textFileDocProvider.saveDocument(null, file, document, true);
-														} catch (CoreException e) {
-															e.printStackTrace();
-															failedSaves.add(file);
-														}
-													} finally {
-														textFileDocProvider.disconnect(file);
-													}
-													monitor.worked(1);
-												}
-											}
-											else if (resource instanceof IContainer) {
+							// temporarily disable incremental building to minimize false errors
+							IWorkspace workspace = ResourcesPlugin.getWorkspace();
+							IWorkspaceDescription workspaceDescription = workspace.getDescription();
+							boolean autoBuilding = workspaceDescription.isAutoBuilding();
+							workspaceDescription.setAutoBuilding(false);
+							try {
+								workspace.setDescription(workspaceDescription);
+							} catch (CoreException e2) {
+								e2.printStackTrace();
+							}
+							try {
+								monitor.beginTask(Messages.TidyUpCodeInBulkAction_ConvertingCode, counter);
+								for (IContainer container : selectedContainers)
+									try {
+										final TextFileDocumentProvider textFileDocProvider = Core.instance().getTextFileDocumentProvider();
+										final List<IFile> failedSaves = new LinkedList<IFile>();
+										container.accept(new IResourceVisitor() {
+											@Override
+											public boolean visit(IResource resource) throws CoreException {
 												if (monitor.isCanceled())
 													return false;
+												if (resource instanceof IFile) {
+													IFile file = (IFile) resource;
+													Script script = Script.get(file, true);
+													if (script != null) {
+														C4ScriptParser parser = new C4ScriptParser(file, script);
+														try {
+															parser.parse();
+														} catch (ParsingException e1) {
+															e1.printStackTrace();
+														}
+														textFileDocProvider.connect(file);
+														try {
+															IDocument document = textFileDocProvider.getDocument(file);
+
+															if (document != null)
+																TidyUpCodeAction.runOnDocument(script, null, parser, document);
+
+															try {
+																textFileDocProvider.setEncoding(document, textFileDocProvider.getDefaultEncoding());
+																textFileDocProvider.saveDocument(null, file, document, true);
+															} catch (CoreException e) {
+																e.printStackTrace();
+																failedSaves.add(file);
+															}
+														} finally {
+															textFileDocProvider.disconnect(file);
+														}
+														monitor.worked(1);
+													}
+												}
+												else if (resource instanceof IContainer)
+													if (monitor.isCanceled())
+														return false;
+												return true;
 											}
-											return true;
-										}
-									});
-									// TODO: do something with failedSaves
+										});
+										// TODO: do something with failedSaves
+									} catch (CoreException e) {
+										e.printStackTrace();
+									}
+								monitor.done();
+							} finally {
+								workspaceDescription.setAutoBuilding(autoBuilding);
+								try {
+									workspace.setDescription(workspaceDescription);
 								} catch (CoreException e) {
 									e.printStackTrace();
 								}
 							}
-							monitor.done();
 						}
 					});
 				} catch (InvocationTargetException e) {
