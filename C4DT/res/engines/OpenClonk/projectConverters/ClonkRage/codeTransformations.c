ec3a24d344769b190877bbbae9ffc2dbcad314f5
Mortimer <mardmol@gmx.de>
2012-10-29 16:15:14 +0100

    More code transformations from CR to OC that would be nice to actually be applicable

--- C4DT/res/engines/OpenClonk/projectConverters/ClonkRage/codeTransformations.c
+++ C4DT/res/engines/OpenClonk/projectConverters/ClonkRage/codeTransformations.c
@@ -1,1 +1,6 @@
-ObjectSetAction($obj$, $action$) => $obj$->SetAction($action$);
+ObjectSetAction($obj$, $action$, $params...$) => $obj$->SetAction($action$, $params...$);
+FindObject2($params...$) => FindObject($params...$);
+LocalN($name$, $obj$) => $obj$->$name.stringValue$;
+$id:IDLiteral$ => $id.definitionName$;
+() => nil;
+Message($msg$, $obj$, $params...$) => $obj$->Message($msg$, $params...$);
