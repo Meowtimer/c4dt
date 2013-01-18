8e657654a124c8e634aa9f487fc8a6d0a63ef270
Mortimer <mardmol@gmx.de>
2013-01-18 09:37:42 +0100

    Resurrect c4dt as a product

--- C4DT/src/net/arctics/clonk/ui/ClonkPerspective.java
+++ C4DT/src/net/arctics/clonk/ui/ClonkPerspective.java
@@ -8,34 +8,22 @@ import org.eclipse.ui.IPageLayout;
 import org.eclipse.ui.IPerspectiveFactory;
 
 public class ClonkPerspective implements IPerspectiveFactory {
-
 	@Override
 	public void createInitialLayout(IPageLayout layout) {
-		// layout.addFastView("org.eclipse.ui.navigator.ProjectExplorer", (float) 0.2);
-		// layout.addView("org.eclipse.ui.navigator.ProjectExplorer", IPageLayout.RIGHT, IPageLayout.DEFAULT_VIEW_RATIO, "navigator");
-
-		//layout.addActionSet(Core.id("ui.actionset")); //$NON-NLS-1$
 		layout.addActionSet(IDebugUIConstants.LAUNCH_ACTION_SET);
-
 		layout.addShowViewShortcut(Core.id("views.EngineDeclarationsView")); //$NON-NLS-1$
-
-		// Get the editor area.
 		String editorArea = layout.getEditorArea();
-
 		// Top left: Resource Navigator view and Bookmarks view placeholder
 		IFolderLayout topLeft = layout.createFolder("topLeft", IPageLayout.LEFT, 0.25f, //$NON-NLS-1$
 			editorArea);
 		topLeft.addView(IPageLayout.ID_PROJECT_EXPLORER);
 		topLeft.addPlaceholder(IPageLayout.ID_BOOKMARKS);
-
-		// Bottom left: Outline view and Property Sheet view
+		// Bottom left: Outline view
 		IFolderLayout bottomLeft = layout.createFolder("bottomLeft", IPageLayout.BOTTOM, 0.50f, //$NON-NLS-1$
 			"topLeft"); //$NON-NLS-1$
 		bottomLeft.addView(IPageLayout.ID_OUTLINE);
-		bottomLeft.addView(Core.id("views.ClonkFolderView")); //$NON-NLS-1$
-		bottomLeft.addView(Core.id("views.ClonkPreviewView")); //$NON-NLS-1$
 
-		// Bottom right: Task List view
+		// Bottom right: Problems
 		layout.addView(IPageLayout.ID_PROBLEM_VIEW, IPageLayout.BOTTOM, 0.66f, editorArea);
 
 		layout.addNewWizardShortcut(Core.id("wizards.NewDefinition")); //$NON-NLS-1$
@@ -43,5 +31,4 @@ public class ClonkPerspective implements IPerspectiveFactory {
 		layout.addNewWizardShortcut(Core.id("wizards.NewScenario")); //$NON-NLS-1$
 		layout.addNewWizardShortcut(Core.id("wizards.NewParticle")); //$NON-NLS-1$
 	}
-
 }
