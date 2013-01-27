7f93471e1874a38e6b93f2b9ba9ab57a13788829
Martin Plicht <mardmol@gmx.de>
2013-01-27 14:28:05 +0100

    cli thingie

--- C4DT/src/net/arctics/clonk/cli/CLI.java
+++ C4DT/src/net/arctics/clonk/cli/CLI.java
@@ -14,6 +14,7 @@ import net.arctics.clonk.parser.ParsingException;
 import net.arctics.clonk.parser.c4script.C4ScriptParser;
 import net.arctics.clonk.util.StreamUtil;
 
+import org.eclipse.core.runtime.Platform;
 import org.eclipse.equinox.app.IApplication;
 import org.eclipse.equinox.app.IApplicationContext;
 
@@ -27,19 +28,22 @@ public class CLI implements IApplication {
 	public static void main(String[] args) throws Exception {
 		new CLI().run(args);
 	}
-	
+
 	public String engine;
 	public String engineConfigurationRoot;
-	
+
 	private int parseOptions(String[] args) {
 		for (int i = 0; i < args.length; i++) {
 			String a = args[i];
+			if (a.equals("-application")) {
+				i++; // ignore standard Equinox application argument
+				continue;
+			}
 			if (a.startsWith("--")) {
 				String option = a.substring(2);
 				++i;
-				if (i >= args.length) {
+				if (i >= args.length)
 					throw new IllegalArgumentException("Value required for " + option);
-				}
 				String value = args[i];
 				try {
 					getClass().getField(option).set(this, value);
@@ -51,20 +55,21 @@ public class CLI implements IApplication {
 		}
 		return args.length;
 	}
-	
+
 	/**
 	 * Main entry point. Will interpret arguments of the form --<option>=<value> as assignment to the instance field <option>
-	 * and the rest of the arguments as <method> <parameters...> 
+	 * and the rest of the arguments as <method> <parameters...>
 	 * @param args Arguments to interpret. Passed from {@link #main(String[])}
 	 */
 	public void run(String[] args) {
+		if (args == null || args.length == 0)
+			args = Platform.getCommandLineArgs();
 		int methodIndex = parseOptions(args);
-		if (methodIndex == args.length) {
+		if (methodIndex == args.length)
 			throw new IllegalArgumentException("Missing command");
-		}
 		String methodName = args[methodIndex];
-		for (Method method : getClass().getMethods()) {
-			if (method.getName().equals(methodName)) {
+		for (Method method : getClass().getMethods())
+			if (method.getName().equals(methodName))
 				try {
 					initialize();
 					method.invoke(this, (Object[])Arrays.copyOfRange(args, methodIndex+1, args.length));
@@ -75,18 +80,15 @@ public class CLI implements IApplication {
 					e.printStackTrace();
 					// fallthrough to invalid command
 				}
-			}
-		}
 		throw new IllegalArgumentException(String.format("Invalid command: '%s'", methodName));
 	}
 
 	private void initialize() {
-		if (engine == null || engineConfigurationRoot == null) {
+		if (engine == null || engineConfigurationRoot == null)
 			throw new IllegalArgumentException("--engine and --engineConfigurationRoot command required");
-		}
 		Core.headlessInitialize(engineConfigurationRoot, engine);
 	}
-	
+
 	/**
 	 * repl interface using c4script expressions
 	 */
@@ -109,11 +111,10 @@ public class CLI implements IApplication {
 			scanner.close();
 		}
 	}
-	
+
 	public void verifyScript(String fileName) {
 		C4ScriptParser parser = new C4ScriptParser(new ExecutableScript(fileName, StreamUtil.stringFromFile(new File(fileName)), new Index() {
 			private static final long serialVersionUID = Core.SERIAL_VERSION_UID;
-
 			@Override
 			public Engine engine() {
 				return Core.instance().activeEngine();
@@ -133,9 +134,6 @@ public class CLI implements IApplication {
 	}
 
 	@Override
-	public void stop() {
-		// TODO Auto-generated method stub
-		
-	}
+	public void stop() {}
 
 }
