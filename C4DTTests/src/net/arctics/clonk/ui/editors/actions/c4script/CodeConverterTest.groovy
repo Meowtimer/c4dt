2cb2c4df45330baa4f9a7c5cf04d57b679cf72a4
Mortimer <mardmol@gmx.de>
2013-03-21 10:04:40 +0100

    Tests: Start CodeConverter test case (does not work since text modification framework of Eclipse needed, which crashes when headless)

---
+++ C4DTTests/src/net/arctics/clonk/ui/editors/actions/c4script/CodeConverterTest.groovy
@@ -0,0 +1,42 @@
+package net.arctics.clonk.ui.editors.actions.c4script;
+
+import static org.junit.Assert.*;
+
+import org.eclipse.jface.text.IDocument;
+import org.junit.Test;
+
+import net.arctics.clonk.TestBase;
+import net.arctics.clonk.parser.ASTNode
+import net.arctics.clonk.parser.Declaration
+import net.arctics.clonk.parser.c4script.C4ScriptParser
+import net.arctics.clonk.parser.c4script.TypeUtil;
+import net.arctics.clonk.parser.c4script.C4ScriptParserTest.Setup
+import net.arctics.clonk.ui.editors.actions.c4script.CodeConverter.ICodeConverterContext;
+
+public class CodeConverterTest extends TestBase {
+	@Test
+	public void testOldStyleToNewStyleConversion() {
+		String source =
+"""
+Initialize:
+	return 1;
+"""
+		def setup = new Setup(source)
+		setup.parsers.each { it.run() }
+		setup.index.refresh()
+		setup.scripts.each { it.generateCaches() }
+		
+		def converter = new CodeConverter() {
+			@Override
+			protected ASTNode performConversion(C4ScriptParser parser, ASTNode node, Declaration owner, ICodeConverterContext context) {
+				node.exhaustiveOptimize(TypeUtil.problemReportingContext(parser.script()))
+			}
+		}
+		converter.runOnDocument(setup.script, setup.parser, [
+			get: { source },
+			getChar: { ndx -> source[ndx] as char },
+			get: { offset, len -> source.substring(offset, offset+len-1) },
+			getLength: { source.length() }
+		] as IDocument)
+	}
+}
