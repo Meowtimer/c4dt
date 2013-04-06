93cd2816f0a51324c317aba9b7e2d83c14e41bc9
Martin Plicht <mardmol@gmx.de>
2013-04-06 21:14:37 +0200

    TypeUnification: Fix common base type unification *sigh*

--- C4DT/src/net/arctics/clonk/parser/c4script/ast/TypeUnification.java
+++ C4DT/src/net/arctics/clonk/parser/c4script/ast/TypeUnification.java
@@ -188,18 +188,20 @@ public class TypeUnification {
 				final List<Script> cda = da.conglomerate();
 				final List<Script> cdb = db.conglomerate();
 				cda.retainAll(cdb);
-				final List<Script> commonBases = new ArrayList<>(cda.size());
-				for (final Script x : cda) {
-					boolean includedByAll = true;
-					for (final Script y : cda)
-						if (x != y && !y.doesInclude(y.index(), x)) {
-							includedByAll = false;
-							break;
-						}
-					if (includedByAll)
-						commonBases.add(x);
+				if (cda.size() > 1) {
+					final List<Script> commonBases = new ArrayList<>(cda.size());
+					for (final Script x : cda) {
+						boolean includedByAll = true;
+						for (final Script y : cda)
+							if (x != y && !y.doesInclude(y.index(), x)) {
+								includedByAll = false;
+								break;
+							}
+						if (includedByAll)
+							commonBases.add(x);
+					}
+					cda.removeAll(commonBases);
 				}
-				cda.removeAll(commonBases);
 				return cda.size() > 0 ? TypeChoice.make(cda) : PrimitiveType.OBJECT.unified();
 			}
 		}
