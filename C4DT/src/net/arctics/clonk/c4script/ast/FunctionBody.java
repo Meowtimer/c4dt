644a4408643f3c5f21c4f9fb3c2456ab29f94b65
Mortimer <mardmol@gmx.de>
2013-06-08 13:52:22 +0200

    FunctionBody: Remove owner field, use parent instead

--- C4DT/src/net/arctics/clonk/c4script/ast/FunctionBody.java
+++ C4DT/src/net/arctics/clonk/c4script/ast/FunctionBody.java
@@ -15,19 +15,19 @@ import org.eclipse.jface.text.Region;
  */
 public class FunctionBody extends BunchOfStatements {
 	private static final long serialVersionUID = Core.SERIAL_VERSION_UID;
-	private final Function owner;
 	public FunctionBody(Function owner, List<ASTNode> statements) {
 		super(statements);
-		this.owner = owner;
+		this.parent = owner;
 	}
 	public FunctionBody(Function owner, ASTNode... statements) {
 		super(statements);
-		this.owner = owner;
+		this.parent = owner;
 	}
 	@Override
-	public Function owner() { return owner; }
+	public Function owner() { return (Function)parent; }
 	@Override
 	public void doPrint(ASTNodePrinter builder, int depth) {
+		final Function owner = owner();
 		if (owner != null && owner.isOldStyle())
 			super.doPrint(builder, depth);
 		else
@@ -35,6 +35,7 @@ public class FunctionBody extends BunchOfStatements {
 	}
 	@Override
 	public IRegion absolute() {
+		final Function owner = owner();
 		if (owner != null && !owner.isOldStyle())
 			return new Region(owner.bodyLocation().start()-1, owner.bodyLocation().getLength()+2);
 		else
