6dc30ad97e46050ca52233bd315797f796bdbe1a
Mortimer <mardmol@gmx.de>
2013-06-13 10:08:09 +0200

    Maybe: Restore for Definition

--- C4DT/src/net/arctics/clonk/c4script/typing/dabble/Maybe.java
+++ C4DT/src/net/arctics/clonk/c4script/typing/dabble/Maybe.java
@@ -6,13 +6,14 @@ import net.arctics.clonk.c4script.typing.IRefinedPrimitiveType;
 import net.arctics.clonk.c4script.typing.IType;
 import net.arctics.clonk.c4script.typing.PrimitiveType;
 import net.arctics.clonk.c4script.typing.TypeChoice;
+import net.arctics.clonk.index.Definition;
 
 public class Maybe extends TypeChoice {
 	private static final long serialVersionUID = Core.SERIAL_VERSION_UID;
 	public Maybe(IType type) { super(type, PrimitiveType.ANY); }
 	public IType maybe() { return left; }
 	public static IType make(IType type) {
-		if (type instanceof IRefinedPrimitiveType)
+		if (type instanceof IRefinedPrimitiveType && !(type instanceof Definition))
 			return type;
 		if (eq(type, PrimitiveType.UNKNOWN))
 			return PrimitiveType.ANY;
