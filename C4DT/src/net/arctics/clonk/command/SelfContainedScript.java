05e3f75af27785a6c3c8bd93dde20c949483b40f
Martin Plicht <mardmol@gmx.de>
2013-08-10 14:44:42 +0200

    SelfcontainedScript: Do not add to passed index's entities, be TopLevelEntity

--- C4DT/src/net/arctics/clonk/command/SelfContainedScript.java
+++ C4DT/src/net/arctics/clonk/command/SelfContainedScript.java
@@ -2,22 +2,24 @@ package net.arctics.clonk.command;
 
 import net.arctics.clonk.Core;
 import net.arctics.clonk.ProblemException;
-import net.arctics.clonk.c4script.ScriptParser;
 import net.arctics.clonk.c4script.Script;
+import net.arctics.clonk.c4script.ScriptParser;
 import net.arctics.clonk.c4script.typing.dabble.DabbleInference;
 import net.arctics.clonk.index.Index;
+import net.arctics.clonk.index.IndexEntity.TopLevelEntity;
 import net.arctics.clonk.util.SelfcontainedStorage;
 
 import org.eclipse.core.resources.IStorage;
 import org.eclipse.core.runtime.NullProgressMonitor;
 
-public class SelfContainedScript extends Script {
+public class SelfContainedScript extends Script implements TopLevelEntity {
 
 	private static final long serialVersionUID = Core.SERIAL_VERSION_UID;
 	protected String script;
 
 	public SelfContainedScript(String name, String script, Index index) {
-		super(index);
+		super(null);
+		this.index = index;
 		setName(name);
 		this.script = script;
 		if (script != null) {
@@ -26,9 +28,9 @@ public class SelfContainedScript extends Script {
 				parser.parse();
 				deriveInformation();
 				new DabbleInference()
-				.configure(index, "")
-				.initialize(null, new NullProgressMonitor(), new Script[] {this})
-				.run();
+					.configure(index, "")
+					.initialize(null, new NullProgressMonitor(), new Script[] {this})
+					.run();
 			} catch (final ProblemException e) {
 				e.printStackTrace();
 			}
