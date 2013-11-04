3b36b251739b83b5125d75825f783977c7ff771b
Martin Plicht <mardmol@gmx.de>
2013-11-04 02:30:37 +0100

    This: Oh, it should print itself

--- C4DT/src/net/arctics/clonk/c4script/ast/This.java
+++ C4DT/src/net/arctics/clonk/c4script/ast/This.java
@@ -2,7 +2,11 @@ package net.arctics.clonk.c4script.ast;
 
 import net.arctics.clonk.Core;
 import net.arctics.clonk.ast.ASTNode;
+import net.arctics.clonk.ast.ASTNodePrinter;
+import net.arctics.clonk.c4script.Keywords;
 
 public class This extends ASTNode {
 	private static final long serialVersionUID = Core.SERIAL_VERSION_UID;
+	@Override
+	public void doPrint(ASTNodePrinter output, int depth) { output.append(Keywords.This); }
 }
