bfa8a1b6ea3726a9f348605390dc90a4d783887f
Martin Plicht <mardmol@gmx.de>
2013-12-14 12:06:55 +0100

    Marker: Implement equals/hashCode (no use whatsoever)

--- C4DT/src/net/arctics/clonk/c4script/Marker.java
+++ C4DT/src/net/arctics/clonk/c4script/Marker.java
@@ -1,9 +1,15 @@
 package net.arctics.clonk.c4script;
 
+import static net.arctics.clonk.util.Utilities.as;
+import static net.arctics.clonk.util.Utilities.eq;
+
+import java.util.Arrays;
+
 import net.arctics.clonk.Problem;
 import net.arctics.clonk.ast.ASTNode;
 import net.arctics.clonk.ast.Declaration;
 import net.arctics.clonk.ast.IASTPositionProvider;
+import net.arctics.clonk.util.Hasher;
 
 import org.eclipse.core.resources.IFile;
 
@@ -33,5 +39,29 @@ public class Marker {
 	@Override
 	public String toString() {
 		return String.format("%s @(%s)", code.toString(), reporter.toString()); //$NON-NLS-1$
+	}
+	@Override
+	public int hashCode() {
+		return new Hasher()
+			.in(code)
+			.in(start)
+			.in(end)
+			.in(severity)
+			.in(args.length)
+			.in(args)
+			.in(scriptFile)
+			.finish();
+	}
+	@Override
+	public boolean equals(Object obj) {
+		final Marker other = as(obj, Marker.class);
+		return
+			other != null &&
+			this.code == other.code &&
+			this.start == other.start &&
+			this.end == other.end &&
+			this.severity == other.severity &&
+			Arrays.equals(this.args, other.args) &&
+			eq(this.scriptFile, other.scriptFile);
 	}
 }
