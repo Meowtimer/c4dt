05d5c2d199eed7fd07a85ca842f09b02fbe849b5
Martin Plicht <mardmol@gmx.de>
2014-03-19 21:29:41 +0100

    DefinitionPack: It will be an IPlaceholderPatternMatchTarget one day

--- C4DT/src/net/arctics/clonk/ini/DefinitionPack.java
+++ C4DT/src/net/arctics/clonk/ini/DefinitionPack.java
@@ -1,20 +1,26 @@
 package net.arctics.clonk.ini;
 
 import net.arctics.clonk.Core;
+import net.arctics.clonk.ast.IPlaceholderPatternMatchTarget;
 import net.arctics.clonk.ini.IniData.IniEntryDefinition;
 
 /**
  * Entry class for those Definition[1-9] entries
  * @author madeen
- *
  */
-public class DefinitionPack extends IniEntryValue {
+public class DefinitionPack extends IniEntryValue implements IPlaceholderPatternMatchTarget {
 	private static final long serialVersionUID = Core.SERIAL_VERSION_UID;
 	private String value;
+	public DefinitionPack(String value) {
+		super();
+		this.value = value;
+	}
 	@Override
 	public String toString() { return value; }
 	@Override
 	public void setInput(final String value, final IniEntryDefinition entryData, final IniUnit context) throws IniParserException {
 		this.value = value;
 	}
+	@Override
+	public String patternMatchingText() { return value; }
 }
