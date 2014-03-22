91c4e07bc2a7f920c3eba48c7873d711ac78aae7
Martin Plicht <mardmol@gmx.de>
2014-03-23 00:30:57 +0100

    CR->OC: Compatibility.c: EvaluateID/UnknownFunction

--- C4DT/res/engines/OpenClonk/projectConverters/ClonkRage/compatibility/System.ocg/Compatibility.c
+++ C4DT/res/engines/OpenClonk/projectConverters/ClonkRage/compatibility/System.ocg/Compatibility.c
@@ -3,3 +3,13 @@ global func NoOp(what)
 	Log("NoOp: %s", what);
 	return true;
 }
+
+global func EvaluateID(string name)
+{
+	return nil; // nope
+}
+
+global func UnknownFunction(string name)
+{
+	Log("Unknown function called: %s", name);
+}
