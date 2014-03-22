21933ef7e3cc4efdfe9504084b4f8ffbc0316f69
Martin Plicht <mardmol@gmx.de>
2014-03-22 12:39:33 +0100

    cr_to_oc: Convert into an openclonk.app shell on ma desktop

--- C4DT/cr_to_oc/cr_to_oc.c
+++ C4DT/cr_to_oc/cr_to_oc.c
@@ -1,9 +1,7 @@
 func Main() {
 	Log("Setting up workspace");
 	setupWorkspace("/Users/madeen/Projects/Clonk/openclonk.git", "/Applications/Games/Clonk");
-	//linkFolderAsProject("/Users/madeen/Desktop/HorrorWorkspace", "Horror", "ClonkRage");
-	//linkFolderAsProject("/Users/madeen/Desktop/HorrorWorkspace.oc", "Horror.oc", "OpenClonk");
-	linkFolderAsProject("/Users/madeen/Desktop/ClonkRage.oc", "ClonkRage.oc", "OpenClonk");
+	linkFolderAsProject("/Users/madeen/Desktop/ClonkRage.oc/openclonk.app/Contents/Resources", "ClonkRage.oc", "OpenClonk");
 	convertProject("ClonkRage", "ClonkRage.oc", {
 		idMap: mapIDToName("ClonkRage", {})
 	});
