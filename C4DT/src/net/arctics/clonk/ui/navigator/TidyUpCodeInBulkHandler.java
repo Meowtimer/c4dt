1114bc7af0b0cab14bc6e9918321a819da7c084e
Martin Plicht <mardmol@gmx.de>
2014-05-18 14:20:44 +0200

    TidyUpCodeInBulkHandler: Silly lambdaification

--- C4DT/src/net/arctics/clonk/ui/navigator/TidyUpCodeInBulkHandler.java
+++ C4DT/src/net/arctics/clonk/ui/navigator/TidyUpCodeInBulkHandler.java
@@ -1,11 +1,14 @@
 package net.arctics.clonk.ui.navigator;
 
+import static net.arctics.clonk.util.StreamUtil.ofType;
+import static net.arctics.clonk.util.Utilities.printingException;
 import static net.arctics.clonk.util.Utilities.runWithoutAutoBuild;
 
 import java.lang.reflect.InvocationTargetException;
-import java.util.Iterator;
-import java.util.LinkedList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import net.arctics.clonk.Core;
 import net.arctics.clonk.ProblemException;
@@ -20,7 +23,6 @@ import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
 import org.eclipse.core.resources.IContainer;
 import org.eclipse.core.resources.IFile;
-import org.eclipse.core.resources.IFolder;
 import org.eclipse.core.resources.IProject;
 import org.eclipse.core.resources.IResource;
 import org.eclipse.core.resources.IResourceVisitor;
@@ -32,15 +34,12 @@ import org.eclipse.ui.PlatformUI;
 import org.eclipse.ui.handlers.HandlerUtil;
 
 public class TidyUpCodeInBulkHandler extends AbstractHandler {
-
 	@Override
 	public boolean isEnabled() {
 		final IStructuredSelection sel = (IStructuredSelection) PlatformUI.getWorkbench().getActiveWorkbenchWindow().getSelectionService().getSelection();
 		return Utilities.allInstanceOf(sel.toArray(), IResource.class);
 	}
-
-	private int counter;
-
+	@SuppressWarnings("unchecked")
 	@Override
 	public Object execute(final ExecutionEvent event) throws ExecutionException {
 		final ISelection selection = HandlerUtil.getCurrentSelection(event);
@@ -48,78 +47,62 @@ public class TidyUpCodeInBulkHandler extends AbstractHandler {
 			if (!UI.confirm(PlatformUI.getWorkbench().getActiveWorkbenchWindow().getShell(), Messages.TidyUpCodeInBulkHandler_ReallyConvert, null))
 				return null;
 			final IStructuredSelection sel = (IStructuredSelection) selection;
-			final Iterator<?> it = sel.iterator();
-			final List<IContainer> selectedContainers = new LinkedList<IContainer>();
-			while (it.hasNext()) {
-				final Object obj = it.next();
+			final List<IContainer> selectedContainers = ((List<Object>)sel.toList()).stream().flatMap(obj -> {
 				if (obj instanceof IProject)
 					try {
 						final IResource[] selectedResources = ((IProject)obj).members(IContainer.EXCLUDE_DERIVED);
-						for(int i = 0; i < selectedResources.length;i++)
-							if (selectedResources[i] instanceof IContainer && !selectedResources[i].getName().startsWith(".")) //$NON-NLS-1$
-								selectedContainers.add((IContainer) selectedResources[i]);
+						return ofType(Arrays.stream(selectedResources), IContainer.class).filter(s -> !s.getName().startsWith(".")); //$NON-NLS-1$
 					}
 					catch (final CoreException ex) {
 						ex.printStackTrace();
+						return Stream.empty();
 					}
-				else if (obj instanceof IFolder)
-					selectedContainers.add((IContainer) obj);
-			}
-			if (selectedContainers.size() > 0) {
+				else if (obj instanceof IContainer)
+					return Arrays.stream(new IContainer[] { (IContainer)obj });
+				else
+					return Stream.empty();
+			}).collect(Collectors.toList());
+			if (!selectedContainers.isEmpty()) {
 				final ProgressMonitorDialog progressDialog = new ProgressMonitorDialog(PlatformUI.getWorkbench().getActiveWorkbenchWindow().getShell());
 				try {
 					progressDialog.run(false, true, monitor -> {
 						// first count how much to do
-						{
-							counter = 0;
-							final IResourceVisitor countingVisitor = resource -> {
+						class CountingVisitor implements IResourceVisitor {
+							public int value = 0;
+							@Override
+							public boolean visit(IResource resource) {
 								if (resource instanceof IFile && Script.get(resource, true) != null)
-									counter++;
+									value++;
 								return true;
 							};
-							for (final IContainer container : selectedContainers)
-								try {
-									container.accept(countingVisitor);
-								} catch (final CoreException e) {
-									e.printStackTrace();
-								}
 						}
-						runWithoutAutoBuild(() -> {
-							monitor.beginTask(Messages.TidyUpCodeInBulkAction_ConvertingCode, counter);
-							for (final IContainer container : selectedContainers)
-								try {
-									container.accept(resource -> {
-										if (monitor.isCanceled())
-											return false;
-										if (resource instanceof IFile) {
-											final IFile file = (IFile) resource;
-											final Script script = Script.get(file, true);
-											if (script != null) {
-												final ScriptParser parser = new ScriptParser(file, script, null);
-												try {
-													parser.parse();
-												} catch (final ProblemException e1) {
-													e1.printStackTrace();
-												}
-												Core.instance().performActionsOnFileDocument(file, document -> {
-													if (document != null)
-														TidyUpCodeAction.converter().runOnDocument(script, document);
-													return null;
-												}, true);
-												monitor.worked(1);
-											}
-										}
-										else if (resource instanceof IContainer)
-											if (monitor.isCanceled())
-												return false;
-										return true;
-									});
-									// TODO: do something with failedSaves
-								} catch (final CoreException e) {
-									e.printStackTrace();
+						final CountingVisitor counter = new CountingVisitor();
+						selectedContainers.forEach(printingException(c -> c.accept(counter), CoreException.class));
+						monitor.beginTask(Messages.TidyUpCodeInBulkAction_ConvertingCode, counter.value);
+						runWithoutAutoBuild(() -> selectedContainers.forEach(printingException(c -> c.accept(resource -> {
+							if (monitor.isCanceled())
+								return false;
+							if (resource instanceof IFile) {
+								final IFile file = (IFile) resource;
+								final Script script = Script.get(file, true);
+								if (script != null) {
+									final ScriptParser parser = new ScriptParser(file, script, null);
+									try {
+										parser.parse();
+									} catch (final ProblemException e1) {
+										e1.printStackTrace();
+									}
+									Core.instance().performActionsOnFileDocument(file, document -> {
+										if (document != null)
+											TidyUpCodeAction.converter().runOnDocument(script, document);
+										return null;
+									}, true);
+									monitor.worked(1);
 								}
-							monitor.done();
-						});
+							}
+							return true;
+						}), CoreException.class)));
+						monitor.done();
 					});
 				} catch (final InvocationTargetException e) {
 					e.printStackTrace();
