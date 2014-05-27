8dbcd90eb706d6b7d21462c7c0f2b040f267fb32
Mortimer <mardmol@gmx.de>
2014-05-28 00:52:11 +0200

    DispatchCase moved

--- C4DT/src/net/arctics/clonk/util/DispatchCase.java
+++ C4DT/src/net/arctics/clonk/util/DispatchCase.java
@@ -1,20 +1,20 @@
-package net.arctics.clonk.c4script.cpp;
+package net.arctics.clonk.util;
 
 import static java.util.Arrays.stream;
 
-class DispatchCase<T, R> {
+public class DispatchCase<T, R> {
 	public final Class<T> cls;
-	public final java.util.function.Function<T, R> fn;
+	public final java.util.function.Function<? super T, R> fn;
 	public DispatchCase(Class<T> cls, java.util.function.Function<T, R> fn) {
 		super();
 		this.cls = cls;
 		this.fn = fn;
 	}
 	public static <T, R> DispatchCase<T, R> caze(Class<T> cls, java.util.function.Function<T, R> fn) {
-		return new DispatchCase<>(cls, fn);
+		return new DispatchCase<T, R>(cls, fn);
 	}
 	@SafeVarargs
-	public static <B, R> R dispatch(B node, DispatchCase<? extends B, R>... fns) {
+	public static <B, R> R dispatch(B node, DispatchCase<?, R>... fns) {
 		@SuppressWarnings("unchecked")
 		final DispatchCase<B, R> fn = stream(fns)
 			.filter(f -> f.cls.isInstance(node))
