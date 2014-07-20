22e34f170e9ebf9ef032427f62567fd149fff227
Mortimer <mardmol@gmx.de>
2014-07-20 14:17:26 +0200

    Format

--- C4DT/src/net/arctics/clonk/c4script/typing/IRefinedPrimitiveType.java
+++ C4DT/src/net/arctics/clonk/c4script/typing/IRefinedPrimitiveType.java
@@ -1,7 +1,5 @@
 package net.arctics.clonk.c4script.typing;
 
-
-
 public interface IRefinedPrimitiveType extends IType {
 	PrimitiveType primitiveType();
 }
--- C4DT/src/net/arctics/clonk/c4script/typing/ReferenceType.java
+++ C4DT/src/net/arctics/clonk/c4script/typing/ReferenceType.java
@@ -6,50 +6,20 @@ import net.arctics.clonk.Core;
 import net.arctics.clonk.util.ArrayUtil;
 
 public class ReferenceType extends WrappedType implements IRefinedPrimitiveType {
-
 	private static final long serialVersionUID = Core.SERIAL_VERSION_UID;
-
 	public ReferenceType(final IType type) { super(type); }
-
-	public IType type() {
-		return wrappedType;
-	}
-	
+	public IType type() { return wrappedType; }
 	@Override
-	public Iterator<IType> iterator() {
-		return ArrayUtil.iterable(PrimitiveType.REFERENCE, wrappedType).iterator();
-	}
-
+	public Iterator<IType> iterator() { return ArrayUtil.iterable(PrimitiveType.REFERENCE, wrappedType).iterator(); }
 	@Override
-	public String typeName(final boolean special) {
-		return String.format("%s %s", wrappedType.toString(), PrimitiveType.REFERENCE.toString());
-	}
-	
+	public String typeName(final boolean special) { return String.format("%s %s", wrappedType.toString(), PrimitiveType.REFERENCE.toString()); }
 	@Override
-	public String toString() {
-		return typeName(false);
-	}
-
+	public String toString() { return typeName(false); }
 	@Override
-	public IType simpleType() {
-		return PrimitiveType.REFERENCE;
-	}
-	
+	public IType simpleType() { return PrimitiveType.REFERENCE; }
 	@Override
-	public boolean equals(final Object obj) {
-		return obj instanceof ReferenceType && ((ReferenceType)obj).wrappedType.equals(wrappedType);
-	}
-	
-	public static IType make(final IType type) {
-		if (type instanceof PrimitiveType)
-			return ((PrimitiveType)type).referenceType();
-		else
-			return type;
-	}
-	
+	public boolean equals(final Object obj) { return obj instanceof ReferenceType && ((ReferenceType)obj).wrappedType.equals(wrappedType); }
+	public static IType make(final IType type) { return type instanceof PrimitiveType ? ((PrimitiveType)type).referenceType() : type; }
 	@Override
-	public PrimitiveType primitiveType() {
-		return PrimitiveType.REFERENCE;
-	}
-
+	public PrimitiveType primitiveType() { return PrimitiveType.REFERENCE; }
 }
