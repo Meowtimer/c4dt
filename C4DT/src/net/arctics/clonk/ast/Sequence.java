6d7985b4ff0b4a59a05f3c5dd2dd3ead1e723368
Martin Plicht <mardmol@gmx.de>
2015-08-28 20:55:45 +0200

    Sequence: toPieces method which is supposed to turn sequence into a linked list structure which is more easily digested

--- C4DT/src/net/arctics/clonk/ast/Sequence.java
+++ C4DT/src/net/arctics/clonk/ast/Sequence.java
@@ -5,6 +5,8 @@ import java.util.Arrays;
 import java.util.List;
 
 import net.arctics.clonk.Core;
+import net.arctics.clonk.c4script.ast.ArrayElementExpression;
+import net.arctics.clonk.c4script.ast.MemberOperator;
 
 public class Sequence extends ASTNodeWithSubElementsArray {
 
@@ -21,14 +23,17 @@ public class Sequence extends ASTNodeWithSubElementsArray {
 			prev = e;
 		}
 	}
+
 	public Sequence(final List<ASTNode> elms) {
 		this(elms.toArray(new ASTNode[elms.size()]));
 	}
+
 	@Override
 	public void doPrint(final ASTNodePrinter output, final int depth) {
 		for (final ASTNode e : elements)
 			e.print(output, depth);
 	}
+
 	public Sequence subSequenceUpTo(final ASTNode elm) {
 		final List<ASTNode> list = new ArrayList<ASTNode>(elements.length);
 		for (final ASTNode e : elements)
@@ -43,6 +48,7 @@ public class Sequence extends ASTNodeWithSubElementsArray {
 		} else
 			return null;
 	}
+
 	public Sequence subSequenceIncluding(final ASTNode elm) {
 		final List<ASTNode> list = new ArrayList<ASTNode>(elements.length);
 		for (final ASTNode e : elements) {
@@ -57,16 +63,19 @@ public class Sequence extends ASTNodeWithSubElementsArray {
 		} else
 			return null;
 	}
+
 	public ASTNode successorOfSubElement(final ASTNode element) {
 		for (int i = 0; i < elements.length; i++)
 			if (elements[i] == element)
 				return i+1 < elements.length ? elements[i+1] : null;
 		return null;
 	}
+
 	@Override
 	public Object evaluate(final IEvaluationContext context) throws ControlFlowException {
 		return lastElement().evaluate(context);
 	}
+
 	@Override
 	public void postLoad(final ASTNode parent) {
 		super.postLoad(parent);
@@ -77,4 +86,38 @@ public class Sequence extends ASTNodeWithSubElementsArray {
 			prev = e;
 		}
 	}
+
+	public enum PieceKind {
+		Property,
+		ArrayElement
+	}
+
+	public static class Piece {
+		public final PieceKind kind;
+		public final ASTNode node;
+		public final Piece previous;
+		public Piece(PieceKind kind, ASTNode node, Piece previous) {
+			super();
+			this.kind = kind;
+			this.node = node;
+			this.previous = previous;
+		}
+	}
+
+	public Piece toPieces() {
+		final ASTNode[] elements = this.subElements();
+		Piece piece = null;
+		PieceKind kind = null;
+		for (final ASTNode node : elements)
+			if (node instanceof MemberOperator)
+				kind = PieceKind.Property;
+			else if (node instanceof ArrayElementExpression)
+				piece = new Piece(PieceKind.ArrayElement, ((ArrayElementExpression) node).argument(), piece);
+			else {
+				piece = new Piece(kind, node, piece);
+				kind = null;
+			}
+		return piece;
+	}
+
 }
