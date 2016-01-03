a784557f1dcc351697b79997be94d8adf924e404
Martin Plicht <mardmol@gmx.de>
2016-01-03 19:30:14 +0100

    Effect: Format

--- C4DT/src/net/arctics/clonk/c4script/effect/Effect.java
+++ C4DT/src/net/arctics/clonk/c4script/effect/Effect.java
@@ -18,8 +18,10 @@ import net.arctics.clonk.c4script.typing.PrimitiveType;
 import net.arctics.clonk.util.ArrayUtil;
 
 public class Effect extends ProplistDeclaration {
+
 	private static final long serialVersionUID = Core.SERIAL_VERSION_UID;
 	private final Map<String, EffectFunction> functions = new HashMap<String, EffectFunction>();
+
 	private static final Map<String, IType> engineDefinedProperties = ArrayUtil.map(false,
 		"CommandTarget", PrimitiveType.OBJECT,
 		"Interval", PrimitiveType.INT,
@@ -27,41 +29,51 @@ public class Effect extends ProplistDeclaration {
 		"Name", PrimitiveType.STRING,
 		"Time", PrimitiveType.INT
 	);
+
 	public Effect(final String name, final Iterable<EffectFunction> functions) {
 		super(new ArrayList<Variable>(5));
 		setName(name);
-		for (final EffectFunction f : functions)
-			addFunction(f);
-		for (final Map.Entry<String, IType> e : engineDefinedProperties.entrySet()) {
-			final Variable v = new Variable(e.getKey(), e.getValue());
+		functions.forEach(this::addFunction);
+		for (final Map.Entry<String, IType> engineDefinedProperty : engineDefinedProperties.entrySet()) {
+			final Variable v = new Variable(engineDefinedProperty.getKey(), engineDefinedProperty.getValue());
 			v.setScope(Scope.VAR);
 			addComponent(v, false);
 		}
 	}
+
 	public Map<String, EffectFunction> functions() { return functions; }
+
 	@Override
 	public Declaration findLocalDeclaration(final String declarationName, final Class<? extends Declaration> declarationClass) {
-		if (Function.class.isAssignableFrom(declarationClass))
+		if (Function.class.isAssignableFrom(declarationClass)) {
 			return functions.get(declarationName);
-		else
+		} else {
 			return null;
+		}
 	}
+
 	public void addFunction(final EffectFunction function) {
 		function.setEffect(this);
 		functions.put(function.callbackName(), function);
 	}
+
 	public static IType[] parameterTypesForCallback(final String callbackName, final Script script, final IType proplistType) {
-		final boolean proplistParameters = script.engine().settings().supportsProplists;
-		if (isAnyOf(callbackName, "Start", "Timer", "Stop"))
-			return new IType[] {PrimitiveType.OBJECT, proplistParameters ? proplistType : PrimitiveType.INT};
-		if (callbackName.equals("Effect"))
-			return new IType[] {PrimitiveType.STRING, PrimitiveType.OBJECT, proplistParameters ? proplistType : PrimitiveType.INT};
-		if (callbackName.equals("Damage"))
-			return new IType[] {PrimitiveType.OBJECT, proplistParameters ? proplistType : PrimitiveType.INT, PrimitiveType.INT, PrimitiveType.INT};
-		return new IType[] {PrimitiveType.OBJECT, proplistParameters ? proplistType : PrimitiveType.INT};
+		final IType effectParameter = script.engine().settings().supportsProplists ? proplistType : PrimitiveType.INT;
+		if (isAnyOf(callbackName, "Start", "Timer", "Stop")) {
+			return new IType[] {PrimitiveType.OBJECT, effectParameter};
+		}
+		if (callbackName.equals("Effect")) {
+			return new IType[] {PrimitiveType.STRING, PrimitiveType.OBJECT, effectParameter};
+		}
+		if (callbackName.equals("Damage")) {
+			return new IType[] {PrimitiveType.OBJECT, effectParameter, PrimitiveType.INT, PrimitiveType.INT};
+		}
+		return new IType[] {PrimitiveType.OBJECT, effectParameter};
 	}
+
 	@Override
 	public String typeName(final boolean special) {
 		return special ? name() : PrimitiveType.PROPLIST.typeName(false);
 	}
+
 }
