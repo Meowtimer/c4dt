6de036008193a56d73cdf6bcf224e77ab9a1d34d
Martin Plicht <mardmol@gmx.de>
2016-01-03 19:31:19 +0100

    Format

--- C4DT/src/net/arctics/clonk/c4script/quickfix/C4ScriptMarkerResolutionGenerator.java
+++ C4DT/src/net/arctics/clonk/c4script/quickfix/C4ScriptMarkerResolutionGenerator.java
@@ -3,18 +3,18 @@ package net.arctics.clonk.c4script.quickfix;
 import java.util.ArrayList;
 import java.util.List;
 
-import net.arctics.clonk.Core;
-import net.arctics.clonk.c4script.Script;
-import net.arctics.clonk.ui.editors.c4script.ScriptQuickAssistProcessor;
-import net.arctics.clonk.ui.editors.c4script.ParameterizedProposal;
-import net.arctics.clonk.ui.editors.c4script.ParameterizedProposalMarkerResolution;
-
 import org.eclipse.core.resources.IMarker;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.jface.text.Position;
 import org.eclipse.jface.text.contentassist.ICompletionProposal;
 import org.eclipse.ui.IMarkerResolution;
 import org.eclipse.ui.IMarkerResolutionGenerator2;
+
+import net.arctics.clonk.Core;
+import net.arctics.clonk.c4script.Script;
+import net.arctics.clonk.ui.editors.c4script.ParameterizedProposal;
+import net.arctics.clonk.ui.editors.c4script.ParameterizedProposalMarkerResolution;
+import net.arctics.clonk.ui.editors.c4script.ScriptQuickAssistProcessor;
 
 public class C4ScriptMarkerResolutionGenerator implements IMarkerResolutionGenerator2 {
 	@Override
@@ -25,17 +25,20 @@ public class C4ScriptMarkerResolutionGenerator implements IMarkerResolutionGener
 		quickAssist.collectProposals(marker, new Position(marker.getAttribute(IMarker.CHAR_START, 0), marker.getAttribute(IMarker.CHAR_END, 0)-marker.getAttribute(IMarker.CHAR_START, 0)),
 			proposals, null, script);
 		final List<IMarkerResolution> res = new ArrayList<IMarkerResolution>(10);
-		for (final ICompletionProposal p : proposals)
-			if (p instanceof ParameterizedProposal)
+		for (final ICompletionProposal p : proposals) {
+			if (p instanceof ParameterizedProposal) {
 				res.add(new ParameterizedProposalMarkerResolution((ParameterizedProposal) p, marker));
+			}
+		}
 		return res.toArray(new IMarkerResolution[res.size()]);
 	}
 	@Override
 	public boolean hasResolutions(final IMarker marker) {
 		try {
-			return
+			return (
 				ScriptQuickAssistProcessor.singleton() != null &&
-				marker.getType().equals(Core.MARKER_C4SCRIPT_ERROR);
+				marker.getType().equals(Core.MARKER_C4SCRIPT_ERROR)
+			);
 		} catch (final CoreException e) {
 			return false;
 		}
