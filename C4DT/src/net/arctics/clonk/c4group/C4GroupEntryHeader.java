950c27e7135d65fbe88de8abf3248d9e68e007cf
Martin Plicht <mardmol@gmx.de>
2016-01-03 19:55:23 +0100

    C4GroupEntryHeader: Final fields, constructor to initialize them

--- C4DT/src/net/arctics/clonk/c4group/C4GroupEntryHeader.java
+++ C4DT/src/net/arctics/clonk/c4group/C4GroupEntryHeader.java
@@ -11,49 +11,56 @@ import net.arctics.clonk.Core;
 public class C4GroupEntryHeader implements Serializable {
 
 	public static final int STORED_SIZE = 316;
-	
+
 	private static final long serialVersionUID = Core.SERIAL_VERSION_UID;
-	
-	private String entryName; //260
-    private boolean packed; // 4
-    private boolean group;
-    private int size;
-    private int entrySize;
-	private int offset;
-	private int time;
-    private boolean hasCRC;
-    private int crc; // unsigned?
-    
-    protected C4GroupEntryHeader() {}
-    
+
+	private final String entryName; //260
+    private final boolean packed; // 4
+    private final boolean group;
+    private final int size;
+    private final int entrySize;
+	private final int offset;
+	private final int time;
+    private final boolean hasCRC;
+    private final int crc; // unsigned?
+
     public C4GroupEntryHeader(final File file) {
-    	entryName = file.getName();
-    	packed = false;
-    	group = file.isDirectory();
+    	this.entryName = file.getName();
+    	this.packed = false;
+    	this.group = file.isDirectory();
     	// GROUPFIXME
     	//group = C4Group.getGroupType(file.getName()) != GroupType.OtherGroup || file.isDirectory();
-    	size = (int) file.length();
-    	offset = 0;
-    	time = (int) file.lastModified();
+    	this.size = (int) file.length();
+    	this.entrySize = 0;
+    	this.offset = 0;
+    	this.time = (int) file.lastModified();
+    	this.hasCRC = false;
+    	this.crc = 0;
     }
-    
-    public static C4GroupEntryHeader createHeader(final String name, final boolean packed, final boolean group, final int size, final int entrySize, final int offset, final int time) {
-    	final C4GroupEntryHeader header = new C4GroupEntryHeader();
-    	header.entryName = name;
-    	header.packed = packed;
-    	header.group = group;
-    	header.size = size;
-    	header.entrySize = entrySize;
-    	header.offset = offset;
-    	header.time = time; 
-    	header.hasCRC = false;
-    	header.crc = 0;
-    	
-    	
+
+    public C4GroupEntryHeader(
+    	final String name,
+    	final boolean packed,
+    	final boolean group,
+    	final int size,
+    	final int entrySize,
+    	final int offset,
+    	final int time,
+    	final boolean hasCRC,
+    	final int crc
+    ) {
+    	entryName = name;
+    	this.packed = packed;
+    	this.group = group;
+    	this.size = size;
+    	this.entrySize = entrySize;
+    	this.offset = offset;
+    	this.time = time;
     	// TODO implement CRC
-    	return header;
+    	this.hasCRC = hasCRC;
+    	this.crc = crc;
     }
-    
+
     public void writeTo(final OutputStream stream) throws IOException {
     	final byte[] buffer = new byte[STORED_SIZE];
     	arrayCopyTo(C4GroupHeader.stringToByte(entryName),buffer,0,260);
@@ -67,36 +74,36 @@ public class C4GroupEntryHeader implements Serializable {
     	arrayCopyTo(C4GroupHeader.int32ToByte(crc),buffer,285);
     	stream.write(buffer, 0, STORED_SIZE);
     }
-    
-    public static C4GroupEntryHeader createFromStream(final InputStream stream) throws C4GroupInvalidDataException {
-    	final C4GroupEntryHeader header = new C4GroupEntryHeader();
-		final byte[] buffer = new byte[STORED_SIZE];
-		try {
-			int readCount = stream.read(buffer,0,STORED_SIZE);
-			while (readCount != STORED_SIZE) {
-				readCount += stream.read(buffer,readCount,STORED_SIZE - readCount);
-			}
-			header.entryName = C4GroupHeader.byteToString(buffer, 0, 260).trim();
-			header.packed = C4GroupHeader.byteToInt32(buffer, 260) > 0;
-			header.group = C4GroupHeader.byteToInt32(buffer, 264) > 0;
-			header.size = C4GroupHeader.byteToInt32(buffer, 268); // size that is used
-			header.entrySize = C4GroupHeader.byteToInt32(buffer, 272); // size that is always 0
-			header.offset = C4GroupHeader.byteToInt32(buffer, 276);
-			header.time = C4GroupHeader.byteToInt32(buffer, 280);
-			header.hasCRC = C4GroupHeader.byteToBoolean(buffer, 284);
-//			if (header.hasCRC == false) {
-//				throw new InvalidDataException("Suspicious entry header: every entry has a CRC except this one.");
-//			}
-			header.crc = C4GroupHeader.byteToInt32(buffer, 285);
 
+    public static C4GroupEntryHeader createFromStream(final InputStream stream) throws C4GroupInvalidDataException {
+		try {
+			final byte[] buffer = readToBuffer(stream, STORED_SIZE);
+			return new C4GroupEntryHeader(
+				C4GroupHeader.byteToString(buffer, 0, 260).trim(),
+				C4GroupHeader.byteToInt32(buffer, 260) > 0,
+				C4GroupHeader.byteToInt32(buffer, 264) > 0,
+				C4GroupHeader.byteToInt32(buffer, 268), // size that is used
+				C4GroupHeader.byteToInt32(buffer, 272), // size that is always 0
+				C4GroupHeader.byteToInt32(buffer, 276),
+				C4GroupHeader.byteToInt32(buffer, 280),
+				C4GroupHeader.byteToBoolean(buffer, 284),
+				C4GroupHeader.byteToInt32(buffer, 285)
+			);
 		} catch (final IOException e) {
 			e.printStackTrace();
-			throw new C4GroupInvalidDataException("There was an IOException."); //$NON-NLS-1$
+			throw new C4GroupInvalidDataException(e.getMessage()); //$NON-NLS-1$
 		}
-		return header;
     }
-    
-	
+
+	private static byte[] readToBuffer(final InputStream stream, int bufferSize) throws IOException {
+		final byte[] buffer = new byte[bufferSize];
+		int readCount = stream.read(buffer,0,bufferSize);
+		while (readCount != bufferSize) {
+			readCount += stream.read(buffer,readCount,bufferSize - readCount);
+		}
+		return buffer;
+	}
+
 	public void skipData(final InputStream stream) throws IOException {
 		if (this.isGroup()) {
 			throw new IOException("skipData for groups not implemented"); //$NON-NLS-1$
@@ -104,68 +111,20 @@ public class C4GroupEntryHeader implements Serializable {
 			stream.skip(size());
 		}
 	}
-    
-    /**
-	 * @return the entryName
-	 */
-	public String entryName() {
-		return entryName;
-	}
-	/**
-	 * @return the packed
-	 */
-	public boolean isPacked() {
-		return packed;
-	}
-	/**
-	 * @return the size
-	 */
-	public int size() {
-		return size;
-	}
-	/**
-	 * @return the entrySize
-	 */
-	public int entrySize() {
-		return entrySize;
-	}
-	/**
-	 * @return the offset
-	 */
-	public int offset() {
-		return offset;
-	}
-	/**
-	 * @return the time
-	 */
-	public int time() {
-		return time;
-	}
-	/**
-	 * @return the hasCRC
-	 */
-	public boolean hasCRC() {
-		return hasCRC;
-	}
 
-	/**
-	 * @return the group
-	 */
-	public boolean isGroup() {
-		return group;
-	}
-
-	/**
-	 * @param offset the offset to set
-	 */
-	public void setOffset(final int offset) {
-		this.offset = offset;
-	}
+	public String entryName() { return entryName; }
+	public boolean isPacked() { return packed; }
+	public int size() { return size; }
+	public int entrySize() { return entrySize; }
+	public int offset() { return offset; }
+	public int time() { return time; }
+	public boolean hasCRC() { return hasCRC; }
+	public boolean isGroup() { return group; }
 
 	private static void arrayCopyTo(final byte[] source, final byte[] target, final int dstOffset) {
     	arrayCopyTo(source, target, dstOffset, source.length);
     }
-    
+
     private static void arrayCopyTo(final byte[] source, final byte[] target, final int dstOffset, final int length) {
     	for(int i = 0;i < length;i++) {
 			if (i >= source.length) {
@@ -175,5 +134,5 @@ public class C4GroupEntryHeader implements Serializable {
 			}
 		}
     }
-    
+
 }
