569501489addd63dadd9bec7a89dfdf4408ed92d
Martin Plicht <mardmol@gmx.de>
2016-05-28 19:43:26 +0200

    FunctionBody: Touch

--- C4DT/src/net/arctics/clonk/c4script/ast/FunctionBody.java
+++ C4DT/src/net/arctics/clonk/c4script/ast/FunctionBody.java
@@ -15,38 +15,40 @@ import net.arctics.clonk.c4script.Function;
  * @author madeen
  */
 public class FunctionBody extends BunchOfStatements {
-	
+
 	private static final long serialVersionUID = Core.SERIAL_VERSION_UID;
-	
+
 	public FunctionBody(final Function owner, final List<ASTNode> statements) {
 		super(statements);
 		this.parent = owner;
 	}
-	
+
 	public FunctionBody(final Function owner, final ASTNode... statements) {
 		super(statements);
 		this.parent = owner;
 	}
-	
+
 	@Override
 	public Function owner() { return (Function)parent; }
-	
+
 	@Override
 	public void doPrint(final ASTNodePrinter builder, final int depth) {
 		final Function owner = owner();
-		if (owner != null && owner.isOldStyle())
+		if (owner != null && owner.isOldStyle()) {
 			super.doPrint(builder, depth);
-		else
+		} else {
 			printBlock(statements(), builder, depth);
+		}
 	}
-	
+
 	@Override
 	public IRegion absolute() {
 		final Function owner = owner();
 		return owner != null && !owner.isOldStyle() ? new Region(owner.bodyLocation().start()-1, owner.bodyLocation().getLength()+2) : super.absolute();
 	}
-	
+
 	public static FunctionBody fromBlock(Block block) {
 		return block instanceof FunctionBody ? (FunctionBody)block : new FunctionBody(null, block.statements());
 	}
+
 }
