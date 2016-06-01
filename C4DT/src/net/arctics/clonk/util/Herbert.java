214a1ef680fa606d1a28b05d9f84ddca35cf3a8a
Martin Plicht <mardmol@gmx.de>
2016-06-01 19:43:07 +0200

    Herbert

--- C4DT/src/net/arctics/clonk/util/Herbert.java
+++ C4DT/src/net/arctics/clonk/util/Herbert.java
@@ -2,6 +2,7 @@ package net.arctics.clonk.util;
 
 import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
+/** http://bugs.java.com/view_bug.do?bug_id=4098033 */
 public interface Herbert<T> {
 	default T clone() { throw new NotImplementedException(); }
 }
