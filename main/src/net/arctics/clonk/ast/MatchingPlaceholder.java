e80e80733f1910a6d6c36424cdeccd5ec628680d
Martin Plicht <mardmol@gmx.de>
2016-07-07 22:28:04 +0200

    MatchingPlaceholder: Space between declarations because ugh

--- main/src/net/arctics/clonk/ast/MatchingPlaceholder.java
+++ main/src/net/arctics/clonk/ast/MatchingPlaceholder.java
@@ -53,37 +53,48 @@ import net.arctics.clonk.util.SelfcontainedStorage;
 import net.arctics.clonk.util.StringUtil;
 
 public class MatchingPlaceholder extends Placeholder {
+
 	private static final long serialVersionUID = Core.SERIAL_VERSION_UID;
 
 	public static class Transformations extends Script {
+
 		protected Transformations(final Index index) {
 			super(index);
 			Command.registerCommandsFromClass(this, this.getClass());
 		}
+
 		private static final long serialVersionUID = Core.SERIAL_VERSION_UID;
+
 		@Override
 		public IStorage source() { return new SelfcontainedStorage("CommandBase", ""); } //$NON-NLS-1$ //$NON-NLS-2$
+
 		@Override
 		public String name() { return "CommandBaseScript"; } //$NON-NLS-1$
+
 		@Override
 		public String nodeName() { return name(); }
+
 		@CommandFunction
 		public static List<ASTNode> reverse(final Object context, final List<ASTNode> input) {
 			final ArrayList<ASTNode> list = new ArrayList<ASTNode>(input);
 			Collections.reverse(list);
 			return list;
 		}
+
 		@CommandFunction
 		public static Object concat(final Object context, final Object a, final Object b) {
 			return a.toString()+b.toString();
 		}
+
 		@CommandFunction
 		public static Object substring(final Object context, final String str, final Long index, final Long length) {
-			if (length != null)
+			if (length != null) {
 				return str.substring(index.intValue(), length.intValue());
-			else
+			} else {
 				return str.substring(index.intValue());
+			}
 		}
+
 		@CommandFunction
 		public static Object eval(final IEvaluationContext context, final ASTNode node) {
 			try {
@@ -93,21 +104,26 @@ public class MatchingPlaceholder extends Placeholder {
 				return null;
 			}
 		}
+
 		@CommandFunction
 		public static String replace(final IEvaluationContext context, final Object text, final Object what, final Object replacement) {
 			return text.toString().replace(what.toString(), replacement.toString());
 		}
+
 		@CommandFunction
 		public static String quote(final IEvaluationContext context, final Object text) {
 			return String.format("\"%s\"", text);
 		}
+
 		@CommandFunction
 		public static void FunctionUsesVarArray(final IEvaluationContext context, ASTNode node) {
 			final Function fn = node.parent(Function.class);
 			final ICodeConverterContext ctx = as(context.self(), ICodeConverterContext.class);
-			if (fn != null && ctx != null)
+			if (fn != null && ctx != null) {
 				ctx.functionUsesVarArray(fn);
+			}
 		}
+
 		@CommandFunction
 		public static ASTNode VarArrayVar(final IEvaluationContext context, ASTNode node, ASTNode index) {
 			final Function fn = node.parent(Function.class);
@@ -118,57 +134,71 @@ public class MatchingPlaceholder extends Placeholder {
 					new AccessVar(ICodeConverterContext.VAR_ARRAY_NAME),
 					new ArrayElementExpression(index)
 				);
-			} else
+			} else {
 				return null;
+			}
 		}
+
 		@CommandFunction
 		public static String EnforceLocal(final IEvaluationContext context, final String name, ASTNode node) {
 			final Function fn = node.parent(Function.class);
 			final ICodeConverterContext ctx = as(context.self(), ICodeConverterContext.class);
 			return fn != null && ctx != null ? ctx.defineFunctionVariable(fn.name(), name) : null;
 		}
+
 		@CommandFunction
 		public static IType Type(final IEvaluationContext context, final ASTNode node) {
 			final Function fn = node.parent(Function.class);
-			if (fn == null)
+			if (fn == null) {
 				return null;
+			}
 			final Script script = fn.parent(Script.class);
-			if (script == null)
+			if (script == null) {
 				return null;
+			}
 			final IType ty = script.typings().get(node);
 			return ty;
 		}
+
 		@CommandFunction
 		public static CallDeclaration Call(final IEvaluationContext context, final String name, final ASTNode[] params) {
 			return new CallDeclaration(name, params);
 		}
+
 		@CommandFunction
 		public static StringLiteral String(final IEvaluationContext context, final String value) {
 			return new StringLiteral(value);
 		}
+
 		@CommandFunction
 		public static AccessVar Var(final IEvaluationContext context, final String name) {
 			return new AccessVar(name);
 		}
+
 		@CommandFunction
 		public static MemberOperator MemberOperator(final IEvaluationContext context, final boolean dot, final boolean tilde, final ID id) {
 			return new MemberOperator(dot, tilde, id, 0);
 		}
+
 		@Override
 		public IVariable variable(final AccessDeclaration access, final Object obj) {
 			if (obj instanceof MatchingPlaceholder || obj instanceof ICodeConverterContext) {
 				final Class<? extends ASTNode> cls = findClass(access.name());
 				return new Constant(cls);
-			} else
+			} else {
 				return null;
+			}
 		}
+
 		private static Map<String, Class<?extends ASTNode>> classCache = new HashMap<>();
+
 		@SuppressWarnings("unchecked")
 		public static Class<? extends ASTNode> findClass(final String className) {
 			synchronized (classCache) {
 				final Class<? extends ASTNode> existing = classCache.get(className);
-				if (existing != null)
+				if (existing != null) {
 					return existing;
+				}
 			}
 			final Map<String, Class<? extends ASTNode>> shortcuts = map(false,
 				"func", Function.class,
@@ -176,8 +206,9 @@ public class MatchingPlaceholder extends Placeholder {
 				"proplist", PropListExpression.class
 			);
 			final Class<? extends ASTNode> scls = shortcuts.get(className);
-			if (scls != null)
+			if (scls != null) {
 				return scls;
+			}
 			final String[] packageFormats = new String[] {
 				"%s.c4script.ast.%s",
 				"%s.parser.%s",
@@ -207,6 +238,7 @@ public class MatchingPlaceholder extends Placeholder {
 				return result;
 			}
 		}
+
 	}
 
 	/**
@@ -214,6 +246,7 @@ public class MatchingPlaceholder extends Placeholder {
 	 * @author madeen
 	 *
 	 */
+
 	public enum Flag {
 		/** Accumulate matches found in function block instead of listing matches on their own */
 		Accumulative
@@ -226,34 +259,50 @@ public class MatchingPlaceholder extends Placeholder {
 		Multiplicity Multiple = m -> true;
 		default Integer absolute() { return this == One ? 1 : null; }
 		default String printed() {
-			if (this == One)
+			if (this == One) {
 				return "#1";
-			else if (this == AtLeastOne)
+			} else if (this == AtLeastOne) {
 				return "+";
-			else if (this == Multiple)
+			} else if (this == Multiple) {
 				return "...";
-			else
+			} else {
 				return "???";
+			}
 		}
 	}
 
 	private Class<? extends ASTNode> requiredClass;
+
 	private Pattern stringRepresentationPattern;
+
 	private Multiplicity multiplicity = Multiplicity.One;
+
 	private ASTNode[] subElements;
+
 	private Function code;
+
 	private Pattern associatedDeclarationNamePattern;
+
 	private String property;
+
 	private EnumSet<Flag> flags;
+
 	private boolean negated;
+
 	private MatchingPlaceholder proto, sub;
 
 	public boolean flagSet(final Flag flag) { return flags != null && flags.contains(flag); }
+
 	public Pattern stringRepresentationPattern() { return stringRepresentationPattern; }
+
 	public Class<? extends ASTNode> requiredClass() { return requiredClass; }
+
 	public Multiplicity multiplicity() { return multiplicity; }
+
 	public String property() { return property; }
+
 	public MatchingPlaceholder proto() { return proto; }
+
 	public void proto(MatchingPlaceholder value) {
 		proto = value;
 		if (value != null) {
@@ -265,7 +314,9 @@ public class MatchingPlaceholder extends Placeholder {
 			this.subElements = value.subElements;
 		}
 	}
+
 	public MatchingPlaceholder sub() { return sub; }
+
 	public void sub(MatchingPlaceholder sub) { this.sub = sub; }
 
 	@Override
@@ -282,8 +333,9 @@ public class MatchingPlaceholder extends Placeholder {
 		final String matchText = original.entryName();
 		final BufferedScanner scanner = new BufferedScanner(matchText);
 		final String entry = scanner.readIdent();
-		if (scanner.peek() == ':')
+		if (scanner.peek() == ':') {
 			scanner.read();
+		}
 		while (!scanner.reachedEOF()) {
 			int start, end;
 			switch (scanner.read()) {
@@ -292,15 +344,17 @@ public class MatchingPlaceholder extends Placeholder {
 				break;
 			case '/':
 				start = scanner.tell(); end = start;
-				while (!scanner.reachedEOF() && scanner.read() != '/')
+				while (!scanner.reachedEOF() && scanner.read() != '/') {
 					end++;
+				}
 				stringRepresentationPattern = Pattern.compile(scanner.readStringAt(start, end));
 				break;
 			case ',':
 				break;
 			case '.':
-				while (scanner.peek() == '.')
+				while (scanner.peek() == '.') {
 					scanner.read();
+				}
 				multiplicity = Multiplicity.Multiple;
 				break;
 			case '+':
@@ -322,19 +376,21 @@ public class MatchingPlaceholder extends Placeholder {
 			}
 			case '!': case '?':
 				String codeString = scanner.readString(scanner.bufferSize()-scanner.tell());
-				if (!codeString.startsWith("{"))
+				if (!codeString.startsWith("{")) {
 					codeString = String.format("return %s;", codeString);
+				}
 				setCode(original, entry, codeString);
 				break;
 			case '^':
 				start = scanner.tell(); end = start;
-				while (!scanner.reachedEOF() && scanner.read() != '^')
+				while (!scanner.reachedEOF() && scanner.read() != '^') {
 					end++;
+				}
 				associatedDeclarationNamePattern = Pattern.compile(scanner.readStringAt(start, end));
 				break;
 			case '>':
 				start = scanner.tell(); end = start;
-				Loop: while (!scanner.reachedEOF())
+				Loop: while (!scanner.reachedEOF()) {
 					switch (scanner.peek()) {
 					case ',': case '!': case '^':
 						break Loop;
@@ -342,6 +398,7 @@ public class MatchingPlaceholder extends Placeholder {
 						scanner.read();
 						end++;
 					}
+				}
 				property = scanner.readStringAt(start, end);
 				break;
 			case '[':
@@ -362,7 +419,7 @@ public class MatchingPlaceholder extends Placeholder {
 			default:
 				scanner.unread();
 				start = scanner.tell(); end = start;
-				Loop: while (!scanner.reachedEOF())
+				Loop: while (!scanner.reachedEOF()) {
 					switch (scanner.peek()) {
 					case ',': case '!':
 						break Loop;
@@ -371,6 +428,7 @@ public class MatchingPlaceholder extends Placeholder {
 						end++;
 						break;
 					}
+				}
 				final String className = scanner.readStringAt(start, end);
 				if (className.length() == 0) {
 					scanner.read();
@@ -381,6 +439,7 @@ public class MatchingPlaceholder extends Placeholder {
 		}
 		this.entryName = entry;
 	}
+
 	protected static Multiplicity makeMultiplicity(final int req_num) {
 		return req_num == 1 ? Multiplicity.One : new Multiplicity() {
 			@Override
@@ -393,6 +452,7 @@ public class MatchingPlaceholder extends Placeholder {
 			public String printed() { return "#"+req_num; }
 		};
 	}
+
 	private boolean setRequiredClass(final String className) throws ProblemException {
 		final Class<? extends ASTNode> cls = Transformations.findClass(className);
 		if (cls != null) {
@@ -403,6 +463,7 @@ public class MatchingPlaceholder extends Placeholder {
 			return false;
 		}
 	}
+
 	private void setCode(final Placeholder original, final String entry, final String codeString) {
 		final Index index = original.parent(Declaration.class).index();
 		final Script transformations = new Transformations(index);
@@ -413,8 +474,9 @@ public class MatchingPlaceholder extends Placeholder {
 			private static final long serialVersionUID = 1L;
 			@Override
 			public boolean gatherIncludes(final Index contextIndex, final Script origin, final Collection<Script> set, final int options) {
-				if (!super.gatherIncludes(contextIndex, origin, set, options))
+				if (!super.gatherIncludes(contextIndex, origin, set, options)) {
 					return false;
+				}
 				set.add(transformations);
 				return true;
 			};
@@ -423,12 +485,13 @@ public class MatchingPlaceholder extends Placeholder {
 
 	public Object transformSubstitution(final Object substitution, final Object context) {
 		final Object[] n = as(substitution, Object[].class);
-		if (n == null)
+		if (n == null) {
 			return null;
+		}
 
 		Stream<Object> stream = stream(n);
 
-		if (property != null)
+		if (property != null) {
 			stream = stream.map(v -> {
 				try {
 					final Callable getter = v == null ? null : ScriptAccessibles.getter(v.getClass(), property);
@@ -438,8 +501,9 @@ public class MatchingPlaceholder extends Placeholder {
 					return v;
 				}
 			});
+		}
 
-		if (code != null)
+		if (code != null) {
 			stream = stream.map(v -> {
 				try {
 					return code.invoke(code.new Invocation(new Object[] {v, this}, code.script(), context));
@@ -448,6 +512,7 @@ public class MatchingPlaceholder extends Placeholder {
 					return v;
 				}
 			});
+		}
 
 		return stream.map(this::pfirsichZitrone).toArray(l -> new ASTNode[l]);
 	}
@@ -465,8 +530,9 @@ public class MatchingPlaceholder extends Placeholder {
 
 	public boolean satisfiedBy(final ASTNode element) {
 		boolean r = internalSatisfied(element);
-		if (negated)
+		if (negated) {
 			r = !r;
+		}
 		return r;
 	}
 
@@ -475,48 +541,58 @@ public class MatchingPlaceholder extends Placeholder {
 			// OC: references to definitions are not IDLiterals but AccessVars referring to proxy variables
 			final AccessVar av = as(element, AccessVar.class);
 			if (requiredClass == IDLiteral.class) {
-				if (av != null && av.declaration() instanceof ProxyVar)
+				if (av != null && av.declaration() instanceof ProxyVar) {
 					break RequiredClass;
+				}
 			}
-			else if (requiredClass == AccessVar.class && av != null && av.declaration() instanceof ProxyVar)
+			else if (requiredClass == AccessVar.class && av != null && av.declaration() instanceof ProxyVar) {
 				return false;
-			if (!requiredClass.isInstance(element))
+			}
+			if (!requiredClass.isInstance(element)) {
 				return false;
+			}
 		}
 		if (stringRepresentationPattern != null) {
 			final IPlaceholderPatternMatchTarget target = as(element, IPlaceholderPatternMatchTarget.class);
 			final String patternMatchingText = target != null ? target.patternMatchingText() : element != null ? element.toString() : null;
-			if (patternMatchingText == null || !stringRepresentationPattern.matcher(patternMatchingText).matches())
+			if (patternMatchingText == null || !stringRepresentationPattern.matcher(patternMatchingText).matches()) {
 				return false;
+			}
 		}
 		if (associatedDeclarationNamePattern != null) {
 			final Declaration decl = associatedDeclaration(element);
-			if (decl == null || !associatedDeclarationNamePattern.matcher(decl.name()).matches())
+			if (decl == null || !associatedDeclarationNamePattern.matcher(decl.name()).matches()) {
 				return false;
+			}
 		}
-		if (code != null)
+		if (code != null) {
 			try {
 				final Object codeResult = code.invoke(code.new Invocation(new Object[] {element}, code.script(), this));
-				if (!eq(Boolean.TRUE, codeResult))
+				if (!eq(Boolean.TRUE, codeResult)) {
 					return false;
+				}
 			} catch (final Exception e) {
 				//System.out.println(e.getMessage());
 				code.invoke(code.new Invocation(new Object[] {element}, code.script(), this));
 				return false;
 			}
-		if (sub != null && !sub.satisfiedBy(element))
+		}
+		if (sub != null && !sub.satisfiedBy(element)) {
 			return false;
+		}
 		return true;
 	}
 
 	protected Declaration associatedDeclaration(final ASTNode element) {
 		final CallDeclaration call = as(element.parent(), CallDeclaration.class);
-		if (call != null)
+		if (call != null) {
 			return call.parmDefinitionForParmExpression(element);
+		}
 
 		final FunctionBody body = as(element, FunctionBody.class);
-		if (body != null)
+		if (body != null) {
 			return body.owner();
+		}
 
 		return null;
 	}
@@ -526,20 +602,26 @@ public class MatchingPlaceholder extends Placeholder {
 		output.append("$");
 		output.append(entryName);
 		final List<String> attribs = new ArrayList<String>(4);
-		if (requiredClass != null)
+		if (requiredClass != null) {
 			attribs.add(requiredClass.getSimpleName());
-		if (stringRepresentationPattern != null)
+		}
+		if (stringRepresentationPattern != null) {
 			attribs.add("/"+stringRepresentationPattern.pattern()+"/");
-		if (property != null)
+		}
+		if (property != null) {
 			attribs.add('>'+property);
-		if (code != null)
+		}
+		if (code != null) {
 			attribs.add('!'+code.body().subElements()[0].subElements()[0].toString());
-		if (multiplicity != Multiplicity.One)
+		}
+		if (multiplicity != Multiplicity.One) {
 			attribs.add(multiplicity.printed());
+		}
 		if (attribs.size() > 0) {
 			output.append(":");
-			if (negated)
+			if (negated) {
 				output.append('~');
+			}
 			output.append(StringUtil.blockString("", "", ",", attribs));
 		}
 		output.append('$');
@@ -548,8 +630,9 @@ public class MatchingPlaceholder extends Placeholder {
 			sub.print(output, depth);
 			output.append(']');
 		}
-		if (subElements != null)
+		if (subElements != null) {
 			Conf.printNodeList(output, subElements, depth, "(", ")");
+		}
 	}
 
 	protected MatchingPlaceholder() { super(""); }
@@ -574,17 +657,20 @@ public class MatchingPlaceholder extends Placeholder {
 			(subElements == null || subElements.length == 0) &&
 			proto == null;
 	}
+
 	public boolean simpleAndMultiplicityOne() {
 		return multiplicity == Multiplicity.One && simple();
 	}
+
 	public boolean consistent(Object value) {
 		final int mult = value instanceof Object[] ? ((Object[])value).length : 1;
 		return multiplicity().absolute() == null || multiplicity().absolute() == mult;
 	}
+
 	@Override
 	protected boolean equalAttributes(ASTNode other_) {
 		final MatchingPlaceholder other = as(other_, MatchingPlaceholder.class);
-		return
+		return (
 			other != null &&
 			super.equalAttributes(other) &&
 			eq(this.associatedDeclarationNamePattern, other.associatedDeclarationNamePattern) &&
@@ -593,6 +679,8 @@ public class MatchingPlaceholder extends Placeholder {
 			eq(this.proto, other.proto) &&
 			eq(this.requiredClass, other.requiredClass) &&
 			eq(this.stringRepresentationPattern, other.stringRepresentationPattern) &&
-			eq(this.subElements, other.subElements);
+			eq(this.subElements, other.subElements)
+		);
 	}
+
 }
